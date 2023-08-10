7e722de8307960f184ba1169a70406f319cfc86a
H-sooyeon <wndqhr__@naver.com>
2023-08-10 15:07:52 +0900

    패션왕 신해빈 조합 수정

--- Solved_ac/패션왕 신해빈.cpp
+++ Solved_ac/패션왕 신해빈.cpp
@@ -1,38 +1,42 @@
 #include <bits/stdc++.h>
 #include <unordered_map>
 using namespace std;
-int test, n;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	unordered_map<string, vector<string>> map;
+	vector<string> key;
+	unordered_map<string, int> map;
 
+	int test, n;
 	cin >> test;
-	string key, value;
+
 	for (int i = 0; i < test; i++) {
 		cin >> n;
-		for (int j = 0; j < n; j++) {
-			cin >> value >> key;
 
-			if (auto iter = map.find(key) == map.end()) {
-				vector<string> v;
-				v.push_back(value);
-				map.insert({ key, v });
-			}
-			else {
-				map[key].push_back(value);
-			}
-
-			// °è»ê
-			for (int j = 0; j < map.size(); j++) {
-
-			}
-		}
+		key.clear();
 		map.clear();
-	};
+
+		string head, value;
+		for (int j = 0; j < n; j++) {
+			cin >> value >> head;
+
+			map[head]++;
+			key.push_back(head);
+		};
+
+		sort(key.begin(), key.end());
+		key.erase(unique(key.begin(), key.end()), key.end());
+
+		int ret = 1;
+		for (int j = 0; j < key.size(); j++) {
+			ret *= (map[key.at(j)] + 1);
+		};
+
+		cout << ret - 1 << "\n";
+	}
 
 	return 0;
 }
