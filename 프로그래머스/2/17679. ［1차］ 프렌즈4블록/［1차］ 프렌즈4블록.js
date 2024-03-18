8d74fe241555ef5125e0b3cfbde472cda62ae2c6
Hwang SooYeon <wndqhr__@naver.com>
2024-03-18 20:23:05 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 3.38 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
@@ -1,56 +1,58 @@
 function solution(m, n, board) {
     let answer = 0;
     board = board.map((row) => Array.from(row));
-    
+        
     while(true) {
-        let find = [];
-        // 현재 위치 기준 앞, 뒤, 위, 아래에 자신과 같은 문자가 존재하는지 체크
-        for(let i = 0; i < m - 1; i++) {
-            for(let j = 0; j < n - 1; j++) {
-                let cur = board[i][j];
-            
-                if(cur && 
-                cur === board[i][j+1] && 
-                cur === board[i+1][j] && 
-                cur === board[i+1][j+1]) {
-                    find.push([i, j]);
+        let popList = [];
+        // 지울 블록 찾기
+        for(let i = 0; i < board.length - 1; i++) {
+            for(let j = 0; j < board[i].length - 1; j++) {
+                if(board[i][j] === 0) continue;
+                if(board[i][j] === board[i][j+1] &&
+                board[i][j] === board[i+1][j] &&
+                board[i][j] === board[i+1][j+1]) {
+                    popList.push([i, j]);
                 }
             }
         }
         
-        if(find.length) {
-            let set = new Set();
-            find.forEach(([y, x]) => {
-                board[y][x] = 0;
-                board[y+1][x] = 0;
-                board[y][x+1] = 0;
-                board[y+1][x+1] = 0;
-                
-                set.add(JSON.stringify([y, x]));
-                set.add(JSON.stringify([y+1, x]));
-                set.add(JSON.stringify([y, x+1]));
-                set.add(JSON.stringify([y+1, x+1]));
-            })
-            
-            answer += set.size;
-        } else break;
+        // 지울 블록이 없으면 break
+        if(popList.length === 0) break;
         
-        for(let y = m-1; y >= 0; y--) {
-            for(let x = 0; x < n; x++) {
-                // 0이 아닌 것 찾기
-                for(let i = y-1; i >= 0; i--) {
-                    if(board[y][x]) break;
-                    
-                    if(board[i][x]) {
-                        board[y][x] = board[i][x];
-                        board[i][x] = 0;
+        // 지우기
+        let set = new Set();
+        for(let [y, x] of popList) {
+            board[y][x] = 0;
+            board[y][x+1] = 0;
+            board[y+1][x] = 0;
+            board[y+1][x+1] = 0;
+            
+            // 지워야 하는 블록 set으로 중복 제거
+            set.add(JSON.stringify([y, x]));
+            set.add(JSON.stringify([y, x+1]));
+            set.add(JSON.stringify([y+1, x]));
+            set.add(JSON.stringify([y+1, x+1]));
+        }
+        
+        answer += set.size;
+        
+        // 블록 내리기
+        for(let i = board.length-1; i >= 0; i--) {
+            for(let j = 0; j < board.length; j++) {
+                // 내려야 하는 경우가 아니면 continue
+                if(board[i][j] !== 0) continue;
+                
+                // 위에서 내릴 수 있는 블록이 있으면 가져오기
+                for(let k = i - 1; k >= 0; k--) {
+                    if(board[k][j] !== 0) {
+                        board[i][j] = board[k][j];
+                        board[k][j] = 0;
                         break;
                     }
                 }
             }
         }
     }
-    
     
     return answer;
 }
