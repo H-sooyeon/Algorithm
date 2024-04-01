bff15a2ba88a6d7ad904ebada42211161220bcbd
Hwang SooYeon <wndqhr__@naver.com>
2024-04-01 19:13:46 +0900

    [level 2] Title: 의상, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,30 +1,20 @@
-// header: yellow_hat, green_turban
-// eyewear: blue_sunglasses
-// 5 = 3 * 2 - 1
-
-// face: corw_mask, blue_sunglasses, smoky_makeup
-// 3
 function solution(clothes) {
     let answer = 1;
     let map = new Map();
     
-    for(let clothe of clothes) {
-        if(map.has(clothe[1])) {
-            map.get(clothe[1]).push(clothe[0]);
+    clothes.forEach(([value, key], i) => {
+        if(map.has(key)) {
+            map.set(key, map.get(key) + 1);
+        } else {
+            map.set(key, 1);
         }
-        else map.set(clothe[1], [clothe[0]]);
-    }
+    })
     
-    // 키 개수 세기
-    if(map.size === 1) {
-        answer = map.get(clothes[0][1]).length;
-    } else {
-        for(let [key, value] of map) {
-            answer *= (value.length + 1);
-        }
-        answer -= 1;
-    }
+    for(let [key, cnt] of map) {
+        answer *= (cnt + 1);
+    };
     
+    answer--;
     
     return answer;
 }
