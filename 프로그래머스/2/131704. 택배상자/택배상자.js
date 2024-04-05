f5b68608e5879f659518930606118ecc22a24de6
Hwang SooYeon <wndqhr__@naver.com>
2024-04-05 11:36:17 +0900

    [level 2] Title: 택배상자, Time: 20.39 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/2/131704. 택배상자/택배상자.js
@@ -0,0 +1,17 @@
+function solution(order) {
+    let answer = 0;
+    let stack = [];
+    
+    for(let i = 1; i <= order.length; i++) {
+        stack.push(i);
+        
+        let length = stack.length;
+        while(length && stack[length - 1] === order[answer]) {
+            answer++;
+            stack.pop();
+            length = stack.length;
+        }
+    }
+    
+    return answer;
+}
