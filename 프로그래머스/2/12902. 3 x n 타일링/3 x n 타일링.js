f15e7fc85202f65e074f528c066b647d62ffa0ee
Hwang SooYeon <wndqhr__@naver.com>
2024-04-09 12:28:52 +0900

    [level 2] Title: 3 x n 타일링, Time: 0.48 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.js
@@ -0,0 +1,18 @@
+function solution(n) {
+    let dp = new Array(n + 1);
+
+    dp[0] = 1;
+    dp[1] = 0;
+    dp[2] = 3;
+    dp[3] = 0;
+    
+    if(n % 2 === 1) return 0;
+    
+    for(let i = 4; i <= n; i+=2) {
+        dp[i] = (4 * dp[i - 2] - dp[i - 4]) % 1000000007;
+        if (dp[i] < 0)
+            dp[i] += 1000000007;
+    }
+
+    return dp[n];
+}
