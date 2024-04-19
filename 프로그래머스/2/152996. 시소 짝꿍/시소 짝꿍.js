4977a9992eb45da9972f57f6edfdb94f4be1973e
dobby <wndqhr__@naver.com>
2024-04-19 12:11:46 +0900

    [level 2] Title: 시소 짝꿍, Time: 0.10 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.js
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.js
@@ -1,22 +1,4 @@
 function solution(weights) {
-    // 비율 1:1 2:4 3:4 3:2인 경우만 존재
-    let answer = 0;
-    let avail = [1/1, 2/4, 3/4, 2/3];
-    let value = {};
-    
-    weights.sort((a, b) => a - b);
-    
-    weights.forEach((v, idx) => {
-        avail.forEach((k, idx) => {
-            let tmp = v * k;
-            if(value[tmp]) {
-                answer += value[tmp];
-            }
-        })
-        
-        if(!value[v]) value[v] = 1;
-        else value[v]++;
-    })
-    
+    var answer = 0;
     return answer;
 }
