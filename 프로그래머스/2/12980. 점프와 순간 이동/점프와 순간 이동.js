22fa7073e1c0a679f223b6c9e9dfef51f4f2959f
dobby <wndqhr__@naver.com>
2024-04-24 09:34:09 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -1,18 +1,19 @@
-// 앞으로 + k, 건전지 사용량 - k
-// 현재 온 거리 x 2, 건전지 사용량 x
-
-// 짝수이면 갈 수 있는 만큼 이동, 홀수면 한 칸 이동
 function solution(n) {
-    let ans = 0;
+    let answer = 0;
     
+    // 도착 지점 -> 시작 지점으로 이동
+    // 도착 지점부터 시작해서 짝수이면 갈 수 있는 만큼 이동
+    // 홀수면 +1
     while(n) {
-        if(n % 2) {
+        if(n % 2 === 1) {
             n--;
-            ans++;
-        } else {
-            n = n / 2;
+            answer++;
+        }
+        else {
+            n /= 2;
         }
     }
     
-    return ans;
+    
+    return answer;
 }
