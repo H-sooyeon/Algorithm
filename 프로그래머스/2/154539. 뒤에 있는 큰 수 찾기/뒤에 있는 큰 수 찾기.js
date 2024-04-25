cf759303c129732752d505caef20f1cee4e03f29
dobby <wndqhr__@naver.com>
2024-04-25 09:31:23 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 561.86 ms, Memory: 232 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
@@ -1,21 +1,32 @@
 function solution(numbers) {
-    let answer = new Array(numbers.length).fill(-1);
+    let answer = [];
+    let stack = [];
     
-    for(let i = numbers.length - 2; i >= 0; i--) {
-        for(let j = i + 1; j < numbers.length; j++) {
-            if(numbers[i] < numbers[j]) {
-                answer[i] = numbers[j];
-                break;
-            }
+    stack.push([numbers[0], 0]);
+    for(let i = 1; i < numbers.length; i++) {
+        let [top, idx] = stack[stack.length - 1];
+        
+        // 뒷 큰수 존재 시 저장
+        while(top < numbers[i] && stack.length) {
+            answer.push([idx, numbers[i]]);
+            stack.pop();
             
-            if(answer[j] === -1) break;
-            
-            if(numbers[i] < answer[j]) {
-                answer[i] = answer[j];
-                break;
+            if(stack.length) {
+                [top, idx] = stack[stack.length - 1];   
             }
         }
+        
+        stack.push([numbers[i], i]);
     }
+    
+    let size = stack.length;
+    for(let i = 0; i < size; i++) {
+        let [top, idx] = stack.pop();
+        answer.push([idx, -1]);
+    }
+
+    answer.sort((a, b) => a[0] - b[0]);
+    answer = answer.map(([idx, v]) => v);
     
     return answer;
 }
