04128e7c8f6e2ef6ebd3ab6428c99de79476f392
dobby <wndqhr__@naver.com>
2024-05-09 10:37:04 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 0.31 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.js
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.js
@@ -1,66 +1,64 @@
 function solution(arrayA, arrayB) {
-    let min1 = 987654321;
-    let min2 = 987654321;
+    let answer = 0;
+    
+    let minA = 987654321;
+    let minB = 987654321;
     
     for(let i = 0; i < arrayA.length; i++) {
-        min1 = Math.min(min1, arrayA[i]);
-        min2 = Math.min(min2, arrayB[i]);
+        minA = Math.min(arrayA[i], minA);
+        minB = Math.min(arrayB[i], minB);
     }
     
-    const getDivisors = (num) => {
-	    const divisors = [];
-	    for(let i = 1; i <= Math.sqrt(num); i++) {
-		    if(num % i === 0) {
-                if(i !== 1)
-			        divisors.push(i);
-			    if(num / i !== i) divisors.push(num / i);
-		    }
-	    }
-        return divisors;
-    }
-    
-    let min1_measure = getDivisors(min1);
-    let min2_measure = getDivisors(min2);
-    
-    let min1_arr = min1_measure.filter(x => !min2_measure.includes(x));
-    let min2_arr = min2_measure.filter(x => !min1_measure.includes(x));
-    
-    min1_arr.sort((a, b) => b - a);
-    min2_arr.sort((a, b) => b - a);
-    
-    let result1 = 0;
-    for(let i = 0; i < min1_arr.length; i++) {
-        let cnt = 0;
-        for(let j = 0; j < arrayA.length; j++) {
-            if(arrayA[j] % min1_arr[i] === 0) {
-                if(arrayB[j] % min1_arr[i] !== 0) {
-                    cnt++;
-                }
-            } else break;
+    // 약수 구하기
+    const getMeasure = (num) => {
+        let result = [];
+        for(let i = 1; i <= Math.sqrt(num); i++) {
+            if(num % i === 0) {
+                if(i !== 1) result.push(i);
+                if(num / i !== i) result.push(num / i);
+            }
         }
-        if(cnt === arrayA.length) {
-            result1 = min1_arr[i];
-            break;
-        }
-        cnt = 0;
-    }
-    
-    let result2 = 0;
-    for(let i = 0; i < min2_arr.length; i++) {
-        let cnt = 0;
-        for(let j = 0; j < arrayA.length; j++) {
-            if(arrayB[j] % min2_arr[i] === 0) {
-                if(arrayA[j] % min2_arr[i] !== 0) {
-                    cnt++;
-                }
-            } else break;
-        }
-        if(cnt === arrayA.length) {
-            result2 = min2_arr[i];
-            break;
-        }
-        cnt = 0;
-    }
         
-    return Math.max(result1, result2);
+        return result;
+    }
+    
+    let measureA = getMeasure(minA);
+    let measureB = getMeasure(minB);
+    
+    let resultA = measureA.filter((v) => !measureB.includes(v));
+    let resultB = measureB.filter((v) => !measureA.includes(v));
+        
+    resultA.sort((a, b) => a - b);
+    resultB.sort((a, b) => a - b);
+    
+    let answerA = 0;
+    let answerB = 0;
+    
+    for(let i = 0; i < resultA.length; i++) {
+        let cntA = 0;
+        for(let j = 0; j < arrayA.length; j++) {            
+            if(arrayA[j] % resultA[i] === 0) {
+                if(arrayB[j] % resultA[i] !== 0)
+                    cntA++;
+            }
+        }
+        
+        if(cntA === arrayA.length) 
+            answerA = resultA[i];
+    }
+    
+    for(let i = 0; i < resultB.length; i++) {
+        let cntB = 0;
+        for(let j = 0; j < arrayB.length; j++) {            
+            if(arrayB[j] % resultB[i] === 0) {
+                if(arrayA[j] % resultB[i] !== 0)
+                    cntB++;
+            }
+        }
+        
+        if(cntB === arrayB.length) 
+            answerB = resultB[i];
+    }
+    
+    return Math.max(answerA, answerB);
 }
