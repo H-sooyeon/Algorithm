2d2c015c7b66c621b79be05223197cd6b5b07c47
dobby <wndqhr__@naver.com>
2024-05-17 09:47:44 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 0.25 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.js
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.js
@@ -1,64 +1,66 @@
 function solution(arrayA, arrayB) {
     let answer = 0;
-    
-    let minA = 987654321;
-    let minB = 987654321;
+    let minA = 0;
+    let minB = 0;
     
     for(let i = 0; i < arrayA.length; i++) {
-        minA = Math.min(arrayA[i], minA);
-        minB = Math.min(arrayB[i], minB);
+        minA = Math.max(arrayA[i], minA);
+        minB = Math.max(arrayB[i], minB);
     }
+
     
-    // 약수 구하기
+    let measureA = [];
+    let measureB = [];
+    
     const getMeasure = (num) => {
         let result = [];
         for(let i = 1; i <= Math.sqrt(num); i++) {
             if(num % i === 0) {
-                if(i !== 1) result.push(i);
-                if(num / i !== i) result.push(num / i);
+                if(i !== 1)
+                    result.push(i);
+                if(num / i !== i) {
+                    result.push(num / i);
+                }
             }
         }
-        
         return result;
     }
     
-    let measureA = getMeasure(minA);
-    let measureB = getMeasure(minB);
-    
-    let resultA = measureA.filter((v) => !measureB.includes(v));
-    let resultB = measureB.filter((v) => !measureA.includes(v));
+    measureA = getMeasure(minA);
+    measureB = getMeasure(minB);
         
-    resultA.sort((a, b) => a - b);
-    resultB.sort((a, b) => a - b);
-    
-    let answerA = 0;
-    let answerB = 0;
-    
-    for(let i = 0; i < resultA.length; i++) {
-        let cntA = 0;
-        for(let j = 0; j < arrayA.length; j++) {            
-            if(arrayA[j] % resultA[i] === 0) {
-                if(arrayB[j] % resultA[i] !== 0)
-                    cntA++;
+    let filterA = measureA.filter((measure) => !measureB.includes(measure));
+    let filterB = measureB.filter((measure) => !measureA.includes(measure));
+        
+    for(let i = 0; i < filterA.length; i++) {
+        let flag = false;
+        for(let j = 0; j < arrayA.length; j++) {
+            if(arrayA[j] % filterA[i]) {
+                flag = true;
+            }
+            
+            if(arrayB[j] % filterA[i] === 0) {
+                flag = true
             }
         }
         
-        if(cntA === arrayA.length) 
-            answerA = resultA[i];
+        if(!flag) answer = Math.max(filterA[i], answer);
     }
     
-    for(let i = 0; i < resultB.length; i++) {
-        let cntB = 0;
-        for(let j = 0; j < arrayB.length; j++) {            
-            if(arrayB[j] % resultB[i] === 0) {
-                if(arrayA[j] % resultB[i] !== 0)
-                    cntB++;
+        for(let i = 0; i < filterB.length; i++) {
+        let flag = false;
+        for(let j = 0; j < arrayB.length; j++) {
+            if(arrayB[j] % filterB[i]) {
+                flag = true;
+            }
+            
+            if(arrayA[j] % filterB[i] === 0) {
+                flag = true
             }
         }
         
-        if(cntB === arrayB.length) 
-            answerB = resultB[i];
+        if(!flag) answer = Math.max(filterB[i], answer);
     }
     
-    return Math.max(answerA, answerB);
+    return answer;
 }
