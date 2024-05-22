ddd59add5a0c41759dc29bc4f6673b93ff8938d6
dobby <wndqhr__@naver.com>
2024-05-22 09:52:51 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,36 +1,29 @@
 function solution(n, words) {
     let answer = [0, 0];
-    
     let set = new Set();
     let people = Array.from(Array(n), () => []);
+    let prev = words[0];
     
-    let words_idx = 1;
-    let flag = false;
-    let people_idx = 1;
     people[0].push(words[0]);
     set.add(words[0]);
-    while(words_idx < words.length) {
-        let word = words[words_idx];
-        // 문자가 안맞을 때
-        if(words[words_idx - 1][words[words_idx - 1].length - 1] !== word[0]) {
-            answer = [people_idx + 1, people[people_idx].length + 1];
-            flag = true;
-            break;
-        }
-        // 단어 중복일 때
-        if(set.has(word)) {
-            answer = [people_idx + 1, people[people_idx].length + 1];
-            flag = true;
-            break;
-        } else {
-            people[people_idx].push(words[words_idx]);
-            set.add(word);
-        }
-        words_idx++;
-        people_idx = (people_idx + 1) % n;
     
-        if(flag) break;
+    for(let i = 1; i < words.length; i++) {
+        if(prev[prev.length - 1] !== words[i][0]) {
+            return [i % n + 1, people[i % n].length + 1];
+        }
+        
+        if(set.has(words[i])) {
+            return [i % n + 1, people[i % n].length + 1];
+        }
+        else {
+            people[i % n].push(words[i]);
+            set.add(words[i]);
+        }
+        prev = words[i];
     }
+    
+    
+    
 
     return answer;
 }
