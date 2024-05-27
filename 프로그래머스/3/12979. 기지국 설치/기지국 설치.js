b11a12be89ddef28135c26ac5d3b2a8bb62dc743
dobby <wndqhr__@naver.com>
2024-05-27 16:19:37 +0900

    [level 3] Title: 기지국 설치, Time: 6.39 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.js
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.js
@@ -1,19 +1,56 @@
 function solution(n, stations, w) {
-    var answer = 0;
-
-    let idx = 1;
-    stations.forEach((station) => {
-        let [start, end] = [station - w, station + w];
-        if(idx > n) {
-            return answer;
+    let answer = 0;
+    let arr = [];
+    
+    let idx = 0;
+    for(let i = 0; i < stations.length; i++) {
+        let start = stations[i] - w;
+        let end = stations[i] + w;
+        
+        if(end > n) end = n;
+        if(start < 1) start = 1;
+        
+        // 겹치는 구간이 있는지 체크
+        if(idx !== 0) {
+            let [prev_start, prev_end] = arr[idx - 1];
+            
+            if(prev_end >= start || prev_end === start - 1) {
+                // 겹치는 구간이 있다면 겹치기
+                arr[idx - 1] = [prev_start, end];
+            }
+            else {
+                arr.push([start, end]);
+                idx++;
+                
+                if(idx !== 1) {
+                    let range = arr[idx - 1][0] - arr[idx - 2][1] - 1;
+                    answer += Math.ceil(range / (w * 2 + 1));
+                }
+                else {
+                    let range = arr[idx - 1][0] - 1;
+                    answer += Math.ceil(range / (w * 2 + 1));
+                }
+            }
         }
-        if(start > idx) {
-            answer += Math.ceil((start - idx)/(2*w+1));
+        else {
+            arr.push([start, end]);
+            idx++;
+            
+            if(idx !== 1) {
+                let range = arr[idx - 1][0] - arr[idx - 2][1] - 1;
+                answer += Math.ceil(range / (w * 2 + 1));
+            }
+            else {
+                let range = arr[idx - 1][0] - 1;
+                answer += Math.ceil(range / (w * 2 + 1));
+            }
         }
-        idx = end+1;
-    })
-    if(idx <= n) {
-        answer += Math.ceil((n - idx + 1)/(2*w+1));
+    };
+    
+    // console.log(arr);
+    if(arr[arr.length - 1][1] < n) {
+        answer += Math.ceil((n - arr[arr.length - 1][1]) / (w * 2 + 1));
     }
+    
     return answer;
 }
