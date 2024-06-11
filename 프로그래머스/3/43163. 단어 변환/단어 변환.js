24998f50417532ac54313e2638ada6d9ddb02d7c
dobby <wndqhr__@naver.com>
2024-06-11 09:29:15 +0900

    [level 3] Title: 단어 변환, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -1,49 +1,36 @@
 function solution(begin, target, words) {
-    let answer = 987654321;
-    
-    // 타겟이 없는 경우
-    if(!words.includes(target)) return 0;
-    
+    let answer = 0;
     let visited = new Array(words.length).fill(false);
     
-    const dfs = (idx, arr, visited) => {
-        if(words[idx] === target) {
-            answer = Math.min(arr.length, answer);
+    const dfs = (word, depth, arr) => {
+        if(word === target) {
+            answer = depth;
             return;
         }
         
         for(let i = 0; i < words.length; i++) {
             if(visited[i]) continue;
+            
+            let last_word = arr[arr.length - 1];
             let cnt = 0;
             
-            for(let j = 0; j < words[i].length; j++) {
-                if(words[i][j] !== words[idx][j]) cnt++;   
+            for(let j = 0; j < last_word.length; j++) {
+                if(last_word[j] !== words[i][j]) cnt++;
                 if(cnt > 1) break;
             }
             
-            if(cnt === 1) {
+            if(cnt <= 1) {
                 arr.push(words[i]);
                 visited[i] = true;
-                dfs(i, arr, visited);
+                dfs(words[i], depth + 1, arr);
                 arr.pop();
                 visited[i] = false;
             }
         }
     }
     
-    for(let i = 0; i < words.length; i++) {
-        let cnt = 0;
-        for(let j = 0; j < words[i].length; j++) {
-            if(begin[j] !== words[i][j]) cnt++;   
-            if(cnt > 1) break;
-        }
-        
-        if(cnt === 1) {
-            visited[i] = true;
-            dfs(i, [words[i]], visited);
-            visited[i] = false;
-        }
-    }
+    dfs(begin, 0, [begin]);
+    
     
     return answer;
 }
