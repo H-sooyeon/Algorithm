3dda8dafdd2f5bae833c0009621aac70a91b341c
dobby <wndqhr__@naver.com>
2024-06-28 14:27:11 +0900

    [level 3] Title: 거스름돈, Time: 49.52 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.js
@@ -0,0 +1,14 @@
+function solution(n, money) {
+    let dp = new Array(n + 1).fill(0);
+    dp[0] = 1;
+    
+    for(let i = 0; i < money.length; i++) {
+        for(let j = 1; j <= n; j++) {
+            if(money[i] <= j) {
+                dp[j] += dp[j - money[i]];
+            }
+        }
+    }
+    
+    return dp[n];
+}
