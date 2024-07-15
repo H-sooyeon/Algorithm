b0818adca55444d28b715e58599aeeb3ab75d173
dobby <wndqhr__@naver.com>
2024-07-16 03:07:25 +0900

    [level 3] Title: 인사고과, Time: 146.62 ms, Memory: 96.6 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/인사고과.js
+++ 프로그래머스/3/152995. 인사고과/인사고과.js
@@ -1,29 +1,49 @@
 function solution(scores) {
     let answer = 1;
-    let wanho = scores[0];
+    let incentive_employee = new Set();
     let wanho_sum = scores[0][0] + scores[0][1];
-    let maxScore = 0;
+    let incentive = [];
+    let employee = [];
     
-    scores.sort((a, b) => {
-        if(a[0] === b[0]) {
-            return a[1] - b[1];
-        }
-        return b[0] - a[0];
+    // key 값으로 사원 번호 할당
+    scores.forEach((v, idx) => {
+        employee.push([idx, ...v]);
     })
-        
-    for(const score of scores) {
-        if(maxScore <= score[1]) {
-            maxScore = score[1];
-            if(score[0] + score[1] > wanho_sum) {
-                answer++;
-            }
+    
+    employee.sort((a, b) => {
+        if(a[1] === b[1]) {
+            return a[2] - b[2];
         }
-        else {
-            if(score[0] >= wanho[0] && score[1] >= wanho[1]) {
-                return -1;
-            }
+        return b[1] - a[1];
+    })
+            
+    let prev = employee[0][2];
+    incentive.push(employee[0]);
+    incentive_employee.add(employee[0][0]);
+    for(let i = 1; i < employee.length; i++) {
+        if(prev > employee[i][2]) {
+            continue;
+        }
+        incentive.push(employee[i]);
+        incentive_employee.add(employee[i][0]);
+        
+        prev = employee[i][2];
+    }
+    
+    // console.log(incentive);
+    // console.log(incentive_employee);
+    
+    if(!incentive_employee.has(0)) {
+        return -1;
+    }
+    
+    for(let i = 0; i < incentive.length; i++) {
+        let sum = incentive[i][1] + incentive[i][2];
+        
+        if (sum > wanho_sum) {
+            answer++;
         }
     }
-            
+    
     return answer;
 }
