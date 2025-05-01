069dbc96a53adf96b9745d1a11637e3b526c23e5
dobby <wndqhr__@naver.com>
2025-05-01 21:18:58 +0900

    [level 3] Title: 등산코스 정하기, Time: 9815.42 ms, Memory: 179 MB -BaekjoonHub

--- 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.js
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.js
@@ -1,179 +1,83 @@
-function PriorityQueue(comparator) {
-  this._comparator = comparator || PriorityQueue.DEFAULT_COMPARATOR;
-  this._elements = [];
+class Queue {
+    constructor() {
+        this.items = {};
+        this.head = 0;
+        this.tail = 0;
+    }
+    push(item) {
+        this.items[this.tail] = item;
+        this.tail += 1;
+    }
+    pop() {
+        delete this.items[this.head];
+        this.head += 1;
+    }
+    size() {
+        return this.tail - this.head;
+    }
+    front() {
+        return this.items[this.head];
+    }
 }
 
-PriorityQueue.DEFAULT_COMPARATOR = function(a, b) {
-  if (typeof a === 'number' && typeof b === 'number') {
-    return a - b;
-  } else {
-    a = a.toString();
-    b = b.toString();
-
-    if (a == b) return 0;
-
-    return (a > b) ? 1 : -1;
-  }
-};
-
-PriorityQueue.prototype.isEmpty = function() {
-  return this.size() === 0;
-};
-
-PriorityQueue.prototype.peek = function() {
-  if (this.isEmpty()) throw new Error('PriorityQueue is empty');
-
-  return this._elements[0];
-};
-
-PriorityQueue.prototype.deq = function() {
-  var first = this.peek();
-  var last = this._elements.pop();
-  var size = this.size();
-
-  if (size === 0) return first;
-
-  this._elements[0] = last;
-  var current = 0;
-
-  while (current < size) {
-    var largest = current;
-    var left = (2 * current) + 1;
-    var right = (2 * current) + 2;
-
-    if (left < size && this._compare(left, largest) >= 0) {
-      largest = left;
-    }
-
-    if (right < size && this._compare(right, largest) >= 0) {
-      largest = right;
-    }
-
-    if (largest === current) break;
-
-    this._swap(largest, current);
-    current = largest;
-  }
-
-  return first;
-};
-
-PriorityQueue.prototype.enq = function(element) {
-  var size = this._elements.push(element);
-  var current = size - 1;
-
-  while (current > 0) {
-    var parent = Math.floor((current - 1) / 2);
-
-    if (this._compare(current, parent) <= 0) break;
-
-    this._swap(parent, current);
-    current = parent;
-  }
-
-  return size;
-};
-
-PriorityQueue.prototype.size = function() {
-  return this._elements.length;
-};
-
-PriorityQueue.prototype.forEach = function(fn) {
-  return this._elements.forEach(fn);
-};
-
-PriorityQueue.prototype._compare = function(a, b) {
-  return this._comparator(this._elements[a], this._elements[b]);
-};
-
-PriorityQueue.prototype._swap = function(a, b) {
-  var aux = this._elements[a];
-  this._elements[a] = this._elements[b];
-  this._elements[b] = aux;
-};
-
 function solution(n, paths, gates, summits) {
-    // 출발지에서 목적지로 가는데 사이클이 없어야 함. (visited)
-    // 목적지에서 출발지로 오는데 사이클이 없어야 함. (visited)
+    let answer = []; // 산봉우리의 번호, intensity 최솟값
+    // path: 등산로 정보, gates: 출입구 번호, summits: 산봉우리
+    const graph = Array.from({length: n + 1}, () => Array());
+    const nodes = new Array(n + 1).fill(0); // 1이면 출입로, 2이면 산봉우리
+    let minSummit = 50001;
+    let minValue = 10000001;
     
-    // 최단거리 알고리즘 적용하면서 가중치 확인 (모든 출발지에 대해 최단거리 알고리즘 실행)
-    let answer = [];
-    let graph = Array.from({length: n + 1}, () => []);
-    
-    paths.forEach((path) => {
-        const [a, b, cost] = path;
+    for(const path of paths) {
+        const [i, j, w] = path;
         
-        graph[a].push([b, cost]);
-        graph[b].push([a, cost]);
-    })
+        graph[i].push([j, w]);
+        graph[j].push([i, w]);
+    }
     
-    summits.forEach((v) => {
-        graph[v] = [];
-    })
+    gates.forEach((gate) => nodes[gate] = 1);
+    summits.forEach((summit) => nodes[summit] = 2);
     
-    let distance = new Array(n + 1).fill(Number.MAX_SAFE_INTEGER);
-    
-    const dijkstra = (start) => {
-        let pq = new PriorityQueue((a, b) => b[0] - a[0]);
+    const bfs = (gate) => {
+        const visited = new Array(n + 1).fill(0);
+        const queue = new Queue();
         
-        // 시작 노드로 가기 위한 최단 거리는 0으로 우선순위 큐에 삽입
-        if(start) {
-            pq.enq([0, start]);
-            distance[start] = 0;
-        } else {
-            gates.forEach((v) => {
-                pq.enq([0, v]);
-                distance[v] = 0;
-            })
-        }
-        
-        while(pq.size()) {
-            let [dist, now] = pq.deq();
+        queue.push([gate, 0]);
+        while(queue.size() > 0) {
+            const [cur, cost] = queue.front();
+            queue.pop();
             
-            if(distance[now] < dist) continue;
+            if(minValue < cost) continue;
             
-            for(let i of graph[now]) {
-                let cost = Math.max(dist, i[1]);
+            for(let [next, time] of graph[cur]) {
+                const intensity = Math.max(time, cost);
                 
-                if(cost < distance[i[0]]) {
-                    distance[i[0]] = cost;
-                    pq.enq([cost, i[0]]);
+                // 출입로라면
+                if(nodes[next] === 1) continue;
+                // 산봉우리라면
+                if(nodes[next] === 2) {
+                    if(minValue > intensity) {
+                        minValue = intensity;
+                        minSummit = next;
+                    }
+                    else if(minValue === intensity && minSummit > next) {
+                        minSummit = next;
+                    }
+                }
+                
+                if(nodes[next] === 0) {
+                    if(visited[next] === 0 || visited[next] > intensity) {
+                        visited[next] = intensity;
+                        queue.push([next, intensity]);
+                    }
                 }
             }
         }
     }
     
-    // 출발지에서 산봉우리까지 가는 객체리스트
-    let route = {};
-        dijkstra();
-        
-        let arrive = summits.map((summit) => {
-            if(route[summit]) {
-                route[summit].push([0, distance[summit]])
-            } else {
-                route[summit] = [[0, distance[summit]]];
-            }
-            
-            return [summit, distance[summit]];
-        });
-    
-    distance = new Array(n + 1).fill(Number.MAX_SAFE_INTEGER);
-    for(let [key, value] of Object.entries(route)) {
-        dijkstra(key);
-        
-        for(let i = 0; i < value.length; i++) {
-            const [start, cost] = value[i];
-            let result = [+key, Math.max(distance[key], cost)];
-            answer.push(result);
-        }
+    for(let gate of gates) {
+        bfs(gate);
     }
-    
-    answer.sort((a, b) => {
-        if(a[1] === b[1]) {
-            return a[0] - b[0];
-        }
-        return a[1] - b[1];
-    })
         
-    return answer[0];
+    return [minSummit, minValue];
 }
