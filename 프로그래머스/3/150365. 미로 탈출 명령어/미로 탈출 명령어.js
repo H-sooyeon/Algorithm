12a010a7eadcd37f39c5d1ed1d7d55d14f8723c0
dobby <wndqhr__@naver.com>
2025-05-03 18:19:02 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 4.03 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.js
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.js
@@ -1,37 +1,39 @@
 function solution(n, m, x, y, r, c, k) {
-    let answer = 'impossible';
-    let dx = [1, 0, 0, -1];
-    let dy = [0, -1, 1, 0];
-    let search = ['d', 'l', 'r', 'u'];
+    let answer = '';
+    // 사전 순으로 빠른 경로 순서 -> d, l, r, u
+    const dx = [1, 0, 0, -1];
+    const dy = [0, -1, 1, 0];
+    const pathSequence = ['d', 'l', 'r', 'u'];
     
-    const dfs = (x, y, cnt, path) => {
-        // 맨헤튼 거리로 남은 횟수 안에 도착지까지 갈 수 없는 경우 stop
-        if(k < cnt + Math.abs(x - r) + Math.abs(y - c)) {
-            return;
-        }
+    const remainDist = (x, y, path) => {
+        return Math.abs(r - x) + Math.abs(c - y) + path.length;
+    }
+    
+    // 장애물이 없는 경로 탐색은 멘헤튼 거리로 탐색하면 된다.
+    let minDist = remainDist(x, y, '');
+    if(minDist > k) return 'impossible';
+    if((k - minDist) % 2 !== 0) return 'impossible';
+    
+    const dfs = (path, x, y) => {
+        const remain = remainDist(x, y, path);
+        if(remain > k || (k - remain) % 2 !== 0 || answer !== '') return;
         
-        // 정답을 찾을 경우
-        if(cnt === k && x === r && y === c) {
-            console.log(path);
+        if(x === r && y === c && k === path.length) {
             answer = path;
             return;
         }
         
         for(let i = 0; i < 4; i++) {
-            let nx = x + dx[i];
-            let ny = y + dy[i];
+            const ny = y + dy[i];
+            const nx = x + dx[i];
             
-            if(ny > m || ny <= 0 || nx > n || nx <= 0) continue;
-            if(answer !== 'impossible') return;
-            
-            dfs(nx, ny, cnt + 1, path + search[i]);
+            if(ny > m || nx > n || ny < 1 || nx < 1) continue;
+            dfs(path + pathSequence[i], nx, ny);
         }
     }
     
-    const min_move = Math.abs(r - x) + Math.abs(c - y);
-    if(min_move > k || (k - min_move) % 2 === 1) return 'impossible';
-    
-    dfs(x, y, 0, '');
-    
-    return answer;
+    dfs('', x, y);
+
+    if(answer) return answer;
+    return 'impossible';
 }
