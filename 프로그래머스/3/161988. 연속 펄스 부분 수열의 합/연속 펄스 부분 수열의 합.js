c7d867831071b09d9644c2f6aec04b2c09b42df9
dobby <wndqhr__@naver.com>
2025-05-04 23:27:26 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 59.21 ms, Memory: 65.2 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.js
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.js
@@ -1,36 +1,42 @@
 function solution(sequence) {
-    let answer = -987654321;
-    let first = new Array(sequence.length);
-    let second = new Array(sequence.length);
+    let answer = 0;
     
-    first[0] = sequence[0] * -1;
-    for(let i = 1; i < first.length; i++) {
-        first[i] = first[i - 1] + sequence[i] * Math.pow(-1, i + 1);
+    const minusSequence = sequence.map((value, idx) => {
+        if(idx % 2 === 0) return value * -1;
+        return value;
+    })
+    
+    const plusSequence = sequence.map((value, idx) => {
+        if(idx % 2 === 0) return value;
+        return value * -1;
+    })
+    
+    for(let i = 1; i < sequence.length; i++) {
+        minusSequence[i] = minusSequence[i-1] + minusSequence[i];
+        plusSequence[i] = plusSequence[i-1] + plusSequence[i];
     }
-    
-    second[0] = sequence[0];
-    for(let i = 1; i < second.length; i++) {
-        second[i] = second[i - 1] + sequence[i] * Math.pow(-1, i);
-    }
-    
-    const subtotal = (arr) => {
+        
+    const findMaxValue = (seq) => {        
         let minIdx = 0;
-        for(let i = 1; i < arr.length; i++) {
-            if(arr[minIdx] > arr[i]) {
+        let maxIdx = 0;
+        
+        let maxValue = seq[0];
+        for(let i = 1; i < seq.length; i++) {
+            maxIdx = i;
+            if(seq[i] < seq[minIdx]) {
                 minIdx = i;
+                maxValue = Math.max(maxValue, seq[minIdx]);
+                continue;
             }
-            answer = Math.max(answer, arr[i] - arr[minIdx]);
-            answer = Math.max(answer, arr[i]);
+            
+            maxValue = Math.max(maxValue, seq[maxIdx] - seq[minIdx], seq[i]);
         }
+        
+        return maxValue;
     }
     
-    if(sequence.length === 1) return Math.max(first[0], second[0]);
-    
-    // console.log(first);
-    // console.log(second);
-    
-    subtotal(first);
-    subtotal(second);
-    
-    return answer;
+    const minusMaxValue = findMaxValue(minusSequence);
+    const plusMaxValue = findMaxValue(plusSequence);
+        
+    return Math.max(minusMaxValue, plusMaxValue);
 }
