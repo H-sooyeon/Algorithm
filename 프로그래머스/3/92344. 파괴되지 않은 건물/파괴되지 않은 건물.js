0ed51bd6c1ba8104c91461a6b055f98ae61c5ab0
dobby <wndqhr__@naver.com>
2025-05-06 11:46:18 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 95.86 ms, Memory: 177 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.js
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.js
@@ -1,48 +1,55 @@
 function solution(board, skill) {
     let answer = 0;
-    let n = board.length;
-    let m = board[0].length;
-    let durability = Array.from({length: n + 1}, () => Array(m + 1).fill(0));
+    // type이 1일 경우 적의 공격
+    // type이 2일 경우 아군의 회복 스킬
+    const n = board.length;
+    const m = board[0].length;
+    const newBoard = Array.from({length: n + 1}, () => Array(m + 1).fill(0));
     
-    skill.forEach((e) => {
-        const [type, r1, c1, r2, c2, degree] = e;
+    skill.forEach((s) => {
+        const [type, r1, c1, r2, c2, degree] = s;
         
         if(type === 1) {
-            durability[r1][c1] -= degree;
-            durability[r2 + 1][c2 + 1] -= degree;
-        
-            durability[r1][c2 + 1] += degree;
-            durability[r2 + 1][c1] += degree;
-        } 
+            // 적의 공격
+            newBoard[r1][c1] -= degree;
+            newBoard[r1][c2 + 1] += degree;
+            
+            newBoard[r2 + 1][c1] += degree;
+            newBoard[r2 + 1][c2 + 1] -= degree;
+        }
         else if(type === 2) {
-            durability[r1][c1] += degree;
-            durability[r2 + 1][c2 + 1] += degree;
-        
-            durability[r1][c2 + 1] -= degree;
-            durability[r2 + 1][c1] -= degree;
+            // 아군의 회복
+            newBoard[r1][c1] += degree;
+            newBoard[r1][c2 + 1] -= degree;
+            
+            newBoard[r2 + 1][c1] -= degree;
+            newBoard[r2 + 1][c2 + 1] += degree;
         }
     })
         
-    // 상하로 누적합
-    for(let j = 0; j < m + 1; j++) {
-        for(let i = 1; i < n + 1; i++) {
-            durability[i][j] += durability[i-1][j];
+    // 가로 누적합
+    for(let i = 0; i <= n; i++) {
+        for(let j = 1; j <= m; j++) {
+            newBoard[i][j] += newBoard[i][j-1];
         }
     }
         
-    // 좌우로 누적합
-    for(let i = 0; i < n + 1; i++) {
-        for(let j = 1; j < m + 1; j++) {
-            durability[i][j] += durability[i][j-1];
+    // 세로 누적합
+    for(let j = 0; j < m; j++) {
+        for(let i = 1; i <= n; i++) {
+            newBoard[i][j] += newBoard[i-1][j];
         }
     }
-        
+    
     for(let i = 0; i < n; i++) {
         for(let j = 0; j < m; j++) {
-            board[i][j] += durability[i][j];
-            if(board[i][j] > 0) answer++;
+            newBoard[i][j] += board[i][j];
+            
+            if(newBoard[i][j] > 0) {
+                answer += 1;
+            }
         }
     }
-        
+    
     return answer;
 }
