eeddf18c00f328709370d2165e5fb8367df34796
dobby <wndqhr__@naver.com>
2025-05-14 15:41:18 +0900

    [level 3] Title: 모두 0으로 만들기, Time: 700.05 ms, Memory: 167 MB -BaekjoonHub

--- 프로그래머스/3/76503. 모두 0으로 만들기/모두 0으로 만들기.js
+++ 프로그래머스/3/76503. 모두 0으로 만들기/모두 0으로 만들기.js
@@ -1,49 +1,73 @@
+class Queue {
+    constructor() {
+        this.items = {};
+        this.head = 0;
+        this.tail = 0;
+    }
+    push(item) {
+        this.items[this.tail] = item;
+        this.tail += 1;
+    }
+    pop() {
+        delete this.items[this.head];
+        this.head += 1;
+    }
+    front() {
+        return this.items[this.head];
+    }
+    size() {
+        return this.tail - this.head;
+    }
+}
+
 function solution(a, edges) {
-    if (a.reduce((a, b) => a + b) !== 0) return -1
+    let answer = 0n;
+    const indegree = new Array(a.length).fill(0);
+    const tree = Array.from({length: a.length}, () => Array());
+    const visited = new Array(a.length).fill(false);
+    const queue = new Queue();
+    const copyA = a.slice();
+
+    // 트리의 모든 점들을 가중치 0으로 만드는 것이 불가능하다면 -1 return
+    const sum = a.reduce((acc, cur) => acc + cur, 0);
+    if(sum !== 0) {
+        return -1;
+    }
     
-    let answer = BigInt(0);
-    let copy = a.slice();
-    let tree = Array.from({length: a.length}, () => []);
-    let visited = new Array(a.length).fill(false);
-    
-    edges.forEach(([e1, e2]) => {
-        tree[e1].push(e2);
-        tree[e2].push(e1);
+    edges.forEach((edge) => {
+        const [u, v] = edge;
+        
+        tree[u].push(v);
+        tree[v].push(u);
+        
+        indegree[u] += 1;
+        indegree[v] += 1;
     })
     
-    let stack = [[0, null]];
-    
-    while(stack.length) {
-        const [node, parent] = stack.pop();
+    for(let i = 0; i < indegree.length; i++) {
+        if(indegree[i] === 1) {
+            queue.push(i);
+        }
+    }
         
-        if(visited[node]) {
-            answer += BigInt(Math.abs(copy[node]));
-            copy[parent] += copy[node];
-            copy[node] = 0;
-            
-            continue;
-        }        
-    
-        stack.push([node, parent]);
-        visited[node] = true;
+    while(queue.size() > 0) {
+        const cur = queue.front();
+        queue.pop();
         
-        for(let next of tree[node]) {
+        visited[cur] = true;
+        answer += BigInt(Math.abs(copyA[cur]));
+
+        for(let next of tree[cur]) {
             if(!visited[next]) {
-                stack.push([next, node]);
+                indegree[next] -= 1;
+                copyA[next] += copyA[cur];
+                copyA[cur] = 0;
+                
+                if(indegree[next] === 1) {
+                    queue.push(next);
+                }
             }
         }
-    }
-    
-    let result = BigInt(copy[0]);
-    for(let i = 1; i < copy.length; i++) {
-        if(copy[i]) {
-            answer += BigInt(Math.abs(copy[i]));
-            result += BigInt(copy[i]);
-        }
-    }
-    
-    if(result) {
-        return -1;
     }
     
     return answer;
