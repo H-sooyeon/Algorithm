872342c968419e8edfa3738c471ad95c3fa53aaa
dobby <wndqhr__@naver.com>
2025-05-23 14:14:32 +0900

    [level 3] Title: 단어 변환, Time: 0.28 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -1,36 +1,37 @@
 function solution(begin, target, words) {
-    let answer = 0;
-    let visited = new Array(words.length).fill(false);
+    let answer = words.length + 1;
     
-    const dfs = (word, depth, arr) => {
-        if(word === target) {
-            answer = depth;
+    if(!words.includes(target)) {
+        return 0;
+    }
+    
+    const dfs = (current, visited, depth) => {
+        if(current === target) {
+            answer = Math.min(depth, answer);
             return;
         }
         
         for(let i = 0; i < words.length; i++) {
             if(visited[i]) continue;
             
-            let last_word = arr[arr.length - 1];
-            let cnt = 0;
-            
-            for(let j = 0; j < last_word.length; j++) {
-                if(last_word[j] !== words[i][j]) cnt++;
-                if(cnt > 1) break;
+            let diff = 0;
+            for(let j = 0; j < words[i].length; j++) {
+                if(words[i][j] !== current[j]) diff += 1;
+                
+                if(diff > 1) {
+                    break;
+                }
             }
             
-            if(cnt <= 1) {
-                arr.push(words[i]);
+            if(diff === 1) {
                 visited[i] = true;
-                dfs(words[i], depth + 1, arr);
-                arr.pop();
+                dfs(words[i], visited, depth + 1);
                 visited[i] = false;
             }
         }
     }
     
-    dfs(begin, 0, [begin]);
-    
+    dfs(begin, new Array(words.length).fill(false), 0);
     
     return answer;
 }
