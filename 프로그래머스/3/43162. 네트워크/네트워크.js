558e6fbb3a903ec770cfe5adef71a7a7375dfe2c
dobby <wndqhr__@naver.com>
2025-05-23 14:34:53 +0900

    [level 3] Title: 네트워크, Time: 0.64 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,34 +1,36 @@
 function solution(n, computers) {
-    let answer = 0;
-    let visited = new Array(n + 1);
-    let list = Array.from(Array(n + 1), () => []);
+    const parents = new Array(computers.length);
     
-    for(let i = 0; i < computers.length; i++) {
-        for(let j = 0; j < computers[i].length; j++) {
+    for(let i = 0; i < n; i++) {
+        parents[i] = i;
+    }
+    
+    const findParent = (x) => {
+        if(x === parents[x]) return x;
+        return parents[x] = findParent(parents[x]);
+    }
+    
+    const unionParents = (a, b) => {
+        const parentA = findParent(a);
+        const parentB = findParent(b);
+        
+        if(parentA < parentB) parents[parentB] = parentA;
+        else parents[parentA] = parentB;
+    }
+    
+    for(let i = 0; i < n; i++) {
+        for(let j = 0; j < n; j++) {
             if(i === j) continue;
             
-            if(computers[i][j]) {
-                list[i + 1].push(j + 1);
-            }
-        }
-    }
-        
-    const dfs = (node) => {
-        visited[node] = true;
-        
-        for(let link of list[node]) {
-            if(!visited[link]) {
-                dfs(link);
+            if(computers[i][j] === 1 && findParent(i) !== findParent(j)) {
+                unionParents(i, j);
             }
         }
     }
     
-    for(let i = 1; i <= n; i++) {
-        if(!visited[i]) {
-            answer++;
-            dfs(i);
-        }
+    for(let i = 0; i < n; i++) {
+        parents[i] = findParent(i);
     }
-    
-    return answer;
+        
+    return new Set(parents).size;
 }
