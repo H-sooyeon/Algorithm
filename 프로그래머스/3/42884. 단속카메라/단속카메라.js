71f4cbdc80c6df2f60b73775a4d04b12ef98a702
dobby <wndqhr__@naver.com>
2025-06-04 14:14:16 +0900

    [level 3] Title: 단속카메라, Time: 31.62 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.js
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.js
@@ -1,16 +1,26 @@
+// 모든 차량이 한 번은 단속용 카메라를 만나도록 하려면 최소 몇 대의 카메라를 설치해야 하는가
 function solution(routes) {
     let answer = 1;
+    const copy = routes.map((route) => [...route]);
     
-    routes.sort((a, b) => a[0] - b[0]);
+    copy.sort((a, b) => {
+        if(a[0] === b[0]) return a[1] - b[1];
+        return a[0] - b[0];
+    })
     
-    let camera = routes[0][1];
-    for(let i = 1; i < routes.length; i++) {
-        if(camera >= routes[i][0] && camera > routes[i][1]) {
-            camera = routes[i][1];
+    let prevEnd = copy[0][1];
+    for(let i = 1; i < copy.length; i++) {
+        const [curStart, curEnd] = copy[i];
+        
+        if(prevEnd >= curStart) {
+            if(prevEnd > curEnd) {
+                prevEnd = curEnd;
+            }
+            continue;
         }
-        if(camera < routes[i][0] && camera < routes[i][1]) {
-            camera = routes[i][1];
-            answer++;
+        else {
+            answer += 1;
+            prevEnd = curEnd;
         }
     }
     
