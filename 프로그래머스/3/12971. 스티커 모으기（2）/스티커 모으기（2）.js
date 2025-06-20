85085fb06c43f7850f4040483a847ac349cb827d
dobby <wndqhr__@naver.com>
2025-06-20 16:38:11 +0900

    [level 3] Title: 스티커 모으기(2), Time: 7.51 ms, Memory: 39.4 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.js
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.js
@@ -1,17 +1,28 @@
 function solution(sticker) {
-    let len = sticker.length + 2;
-    let dp1 = new Array(len).fill(0);
-    let dp2 = new Array(len).fill(0);
+    const len = sticker.length;
+    const firstStart = new Array(len).fill(0);
+    const secondStart = new Array(len).fill(0);
     
-    if(sticker.length === 1) return sticker[0];
+    if(sticker.length <= 2) {
+        if(sticker.length === 1) return sticker[0];
+        return Math.max(sticker[0], sticker[1]);
+    }
     
-    for(let i = 2; i < len-1; i++) {
-        dp1[i] = Math.max(dp1[i-1], dp1[i-2] + sticker[i-2]);
+    firstStart[0] = sticker[0];
+    firstStart[1] = sticker[0];
+    
+    secondStart[1] = sticker[1];
+    secondStart[2] = sticker[1];
+    
+    // firstStart
+    for(let i = 2; i < len - 1; i++) {
+        firstStart[i] = Math.max(firstStart[i-1], firstStart[i-2] + sticker[i]);
+    }
+    
+    // secondStart
+    for(let i = 2; i < len; i++) {
+        secondStart[i] = Math.max(secondStart[i-1], secondStart[i-2] + sticker[i]);
     }
 
-    for(let i = 3; i < len; i++) {
-        dp2[i] = Math.max(dp2[i-1], dp2[i-2] + sticker[i-2]);
-    }
-
-    return Math.max(dp1[len - 2], dp2[len - 1]);
+    return Math.max(firstStart[len-2], secondStart[len-1]);
 }
