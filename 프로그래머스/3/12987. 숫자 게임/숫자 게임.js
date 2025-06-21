bda037e73460c0cc6212f946a8c4ff02d09d0b02
dobby <wndqhr__@naver.com>
2025-06-21 10:15:11 +0900

    [level 3] Title: 숫자 게임, Time: 89.18 ms, Memory: 44.8 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.js
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.js
@@ -1,17 +1,23 @@
 function solution(A, B) {
     let answer = 0;
+    let indexA = 0;
+    let indexB = 0;
     
     A.sort((a, b) => a - b);
     B.sort((a, b) => a - b);
     
-    let aP = 0;
-    let bP = 0;
-    while(bP !== B.length) {
-        if(A[aP] < B[bP]) {
-            aP++;
-            answer++;
+    while(indexA < A.length && indexB < B.length) {
+        if(A[indexA] < B[indexB]) {
+            answer += 1;
+            indexA += 1;
+            indexB += 1;
         }
-        bP++;
+        else if(A[indexA] > B[indexB]) {
+            indexB += 1;
+        }
+        else {
+            indexB += 1;
+        }
     }
     
     return answer;
