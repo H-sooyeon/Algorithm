a9b46d9168b8852e4e705f3e6359f3cd57bfc5c4
dobby <wndqhr__@naver.com>
2025-07-01 22:39:02 +0900

    [level 2] Title: 의상, Time: 0.30 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -2,6 +2,7 @@ function solution(clothes) {
     let answer = 1;
     let map = new Map();
     
+    // map으로 각 분류별 개수 세기
     clothes.forEach(([value, key], i) => {
         if(map.has(key)) {
             map.set(key, map.get(key) + 1);
@@ -10,10 +11,12 @@ function solution(clothes) {
         }
     })
     
+    // 사용하는 경우(총 개수) + 사용하지 않는 경우
     for(let [key, cnt] of map) {
         answer *= (cnt + 1);
     };
     
+    // 모두 다 사용하지 않는 경우 빼주기
     answer--;
     
     return answer;
