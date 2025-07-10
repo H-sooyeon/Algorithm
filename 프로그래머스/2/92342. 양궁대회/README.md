ad63411ed736bddfc4f8fe616a55998c4f28a942
dobby <wndqhr__@naver.com>
2025-07-10 14:26:10 +0900

    [level 2] Title: 양궁대회, Time: 2.88 ms, Memory: 35.6 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/README.md
+++ 프로그래머스/2/92342. 양궁대회/README.md
@@ -1,10 +1,10 @@
 # [level 2] 양궁대회 - 92342 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92342#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92342) 
 
 ### 성능 요약
 
-메모리: 36.8 MB, 시간: 4.72 ms
+메모리: 35.6 MB, 시간: 2.88 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 20일 14:09:47
+2025년 07월 10일 14:26:07
 
 ### 문제 설명
 
--- 프로그래머스/2/92342. 양궁대회/양궁대회.js
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.js
@@ -1,89 +1,58 @@
 function solution(n, info) {
     let answer = [];
-    let answer_cnt = 0;
-    let answer_score = 0;
-    let answer_aScore = 0;
+    let diff = 0;
     
-    const dfs = (b, start, k) => {
-        if(b.length === k) {
-            let tmp = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-            b.forEach((v) => (tmp[v] = info[v] + 1));
+    const dfs = (wins, depth) => {
+        if(depth === 10) {
+            let arrow = 0;
+            const lion = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+            let lionScore = 0;
+            let apeachScore = 0;
             
-            let cnt = 0;
-            let score = 0;
-            let aScore = 0;
+            wins.forEach((win) => {
+                lion[win] = info[win] + 1;
+                arrow += info[win] + 1;
+            })
             
-            for(let i = 0; i < info.length; i++) {
-                cnt += tmp[i];
-                if(info[i] < tmp[i]) {
-                    score += (10 - i);
-                }
-                else if(info[i] >= tmp[i] && info[i] !== 0) {
-                    aScore += (10 - i);
-                }
-                if(cnt > n) return;
+            if(arrow > n) return;
+            lion[10] = n - arrow;
+            
+            lion.forEach((lionArrowCnt, idx) => {
+                if(lionArrowCnt > info[idx]) lionScore += 10 - idx;
+                else if(lionArrowCnt <= info[idx] && info[idx] !== 0) apeachScore += 10 - idx;
+            })
+            
+            if(lionScore - apeachScore > diff) {
+                diff = lionScore - apeachScore;
+                answer = [...lion];
             }
-            
-            if(aScore < score) {
-                // console.log(tmp);
-                // console.log(aScore, score);
-                let a = Math.abs(aScore - score);
-                let b = Math.abs(answer_aScore - answer_score);
-                if(a > b) {
-                    // console.log(tmp);
-                    // console.log(aScore, score, answer_aScore, answer_score);
-                    answer_cnt = cnt;
-                    answer_aScore = aScore;
-                    answer_score = score;
-                    answer = tmp;
-                }
-                else if (a === b) {
-                    // 가장 큰 점수 차이가 여러가지일 경우 가장 낮은 점수를
-                    // 더 많이 맞힌 경우로
-                    let sortArr = [tmp, answer];
-                    sortArr.sort((a, b) => {
-                        for(let i = 10; i >= 0; i--) {
-                            if(a[i] !== b[i]) {
-                                return b[i] - a[i];
-                            }
-                        }
-                        return 0;
-                    })
-                    
-                    if(sortArr[0] === tmp) {
-                        answer = tmp;
-                        answer_score = score;
-                        answer_aScore = aScore;
-                        answer_cnt = cnt;
+            else if(lionScore - apeachScore === diff) {
+                // 낮은 점수가 더 많은 걸로 교체
+                for(let i = lion.length - 1; i >= 0; i--) {
+                    if(lion[i] > answer[i]) {
+                        answer = [...lion];
+                        break;
                     }
-                    // console.log(sortArr[0]);
-                    // console.log(tmp);
-                    // console.log(answer);
-                    // console.log(aScore, score, answer_aScore, answer_score);
-                    // if(answer_score > score) {
-                    //     answer = tmp;
-                    //     answer_score = score;
-                    //     answer_aScore = aScore;
-                    //     answer_cnt = cnt;
-                    // }
+                    else if(lion[i] < answer[i]) {
+                        break;
+                    }
                 }
             }
+            
             return;
         }
         
-        for(let i = start + 1; i < info.length; i++) {
-            b.push(i);
-            dfs(b, i, k);
-            b.pop();
-        }
+        // depth에 대해 lion이 이기는 경우, 지는 경우로 나눠 재귀호출
+        wins.push(depth);
+        dfs(wins, depth + 1);
+    
+        wins.pop();
+        dfs(wins, depth + 1);
     }
     
-    for(let i = 1; i < info.length; i++) {
-        dfs([], -1, i);
-    }
+    dfs([], 0);
     
-    if(!answer.length) return [-1];
+    if(answer.length === 0) return [-1];
     
-    if(answer_cnt < n) answer[10] = n - answer_cnt;
     return answer;
 }
