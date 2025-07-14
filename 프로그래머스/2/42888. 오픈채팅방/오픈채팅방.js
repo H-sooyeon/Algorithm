c70bab61bf614a3d95cb172bebe7b76a1699d4b3
dobby <wndqhr__@naver.com>
2025-07-14 23:54:54 +0900

    [level 2] Title: 오픈채팅방, Time: 284.82 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
@@ -2,29 +2,24 @@ function solution(record) {
     let answer = [];
     let map = new Map();
     
-    record.forEach((v, idx) => {
-        let [state, uid, name] = v.split(' ');
-        if(state !== 'Leave') {
+    record.forEach((v) => {
+        let arr = v.split(' ');
+        if(arr[0] === 'Enter' || arr[0] === 'Change') {
+            let [status, uid, name] = v.split(' ');
             map.set(uid, name);
         }
     })
     
-    for(let i = 0; i < record.length; i++) {
-        let [state, uid, name] = record[i].split(' ');
+    record.forEach((v) => {
+        let arr = v.split(' ');
         
-        if(state === 'Change') continue;
-        
-        let state_str = '';
-        switch(state) {
-            case 'Enter':
-                state_str = '들어왔습니다.';
-                break;
-            case 'Leave':
-                state_str = '나갔습니다.';
+        let lastName = map.get(arr[1]);
+        if(arr[0] === 'Enter') {
+            answer.push(`${lastName}님이 들어왔습니다.`);
+        } else if(arr[0] === 'Leave') {
+            answer.push(`${lastName}님이 나갔습니다.`)
         }
+    })
         
-        answer.push(`${map.get(uid)}님이 ${state_str}`);
-    }
-    
     return answer;
 }
