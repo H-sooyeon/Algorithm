0eea36517b2e7af8a56db94f441c8c384262b800
dobby <wndqhr__@naver.com>
2026-02-16 13:28:57 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,31 +1,5 @@
-function solution(s) {
-    let answer = '';
-    
-    /*
-    * split으로 단어를 구분
-    * 모든 단어를 소문자로 변환
-    * 순회하면서 단어의 첫 글자가 typeof Number가 아니라면 대문자로 변환
-    */
-    
-    const arr = s.split(' ');
-    
-    const result = [];
-    arr.forEach((value) => {
-        if(!value) {
-            result.push([]);
-            return;
-        }
-        
-        if(typeof value[0] === 'Number') {
-            result.push(value.toLowerCase());
-            return;
-        }
-        
-        const tmp = value.toLowerCase().slice(1);
-        result.push(`${value[0].toUpperCase()}${tmp}`);
-    })
-    
-    answer = result.join(' ');
+function solution(s) {    
+    const answer = s.split(' ').map((value) => value.charAt(0).toUpperCase() + value.slice(1).toLowerCase()).join(' ');
     
     return answer;
 }
