1a8a7466921460dc76db02ede57a53357649a73c
dobby <wndqhr__@naver.com>
2026-02-18 17:01:55 +0900

    [level 2] Title: 구명보트, Time: 32.46 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,24 +1,19 @@
 function solution(people, limit) {
-    var answer = 0;
+    let answer = 0;
     
-    people.sort((a,b) => b - a);
+    people.sort((a, b) => a - b);
     
-    let left = 0, right = people.length - 1;
-    while(left < right) {
+    let left = 0;
+    let right = people.length - 1;
+    
+    while(left <= right) {
         if(people[left] + people[right] <= limit) {
-            left++;
-            right--;
-        } else {
-            left++;
+            left += 1;
         }
-        answer++;
+        
+        right -= 1;
+        answer += 1;
     }
-    
-    console.log('left: ', left);
-    console.log('left people:', people[left]);
-    console.log('right: ', right);
-    console.log('right people:', people[right]);
-    if(left === right) answer++;
     
     return answer;
 }
