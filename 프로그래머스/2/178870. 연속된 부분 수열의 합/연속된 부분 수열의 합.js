eb75ab34daa54bed4a9f4377631fa9b7b6dda083
dobby <wndqhr__@naver.com>
2026-03-03 23:03:41 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 42.58 ms, Memory: 65.7 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.js
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.js
@@ -1,48 +1,28 @@
 function solution(sequence, k) {
     let answer = [];
-    let prefix = [];
-    let frontPoint = sequence.length - 2;
-    let backPoint = sequence.length - 1;
-    
-    let result = sequence.reduce((acc, cur) => {
-        prefix.push(acc);
-        return acc + cur;
-    }, 0);
-    
-    prefix.shift();
-    
-    prefix.push(result);
-    while(frontPoint > -1) {
-        if(prefix[backPoint] === k) {
-            answer.push([0, backPoint]);
-            frontPoint--;
-            backPoint--;
-            continue;
-        };
+    let sumLen = sequence.length;
         
-        if(prefix[backPoint] - prefix[frontPoint] === k) {
-            answer.push([frontPoint+1, backPoint]);
-            backPoint--;
+    let left = 0;
+    let right = 0;
+    let sum = sequence[0];
+    
+    while(left <= right) {
+        if(sum === k) {
+            if(sumLen > right - left) {
+                answer = [left, right];
+                sumLen = right - left;
+            }
+        }
+        
+        if(right + 1 < sequence.length && sum < k) {
+            right += 1;
+            sum += sequence[right];
             continue;
         }
         
-        if(prefix[backPoint] - prefix[frontPoint] > k) {
-            backPoint--;
-            continue;
-        }
-        else {
-            frontPoint--;
-            continue;
-        }
+        sum -= sequence[left];
+        left += 1;
     }
     
-    let idx = answer.length - 1;
-    for(let i = answer.length - 2; i >= 0; i--) {
-        let v1 = answer[idx][1] - answer[idx][0];
-        let v2 = answer[i][1] - answer[i][0];
-        
-        if(v1 > v2) idx = i;
-    }
-        
-    return answer[idx];
+    return answer;
 }
