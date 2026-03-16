4c33cef562d074dd93c5e601912492a9e78fd9f8
dobby <wndqhr__@naver.com>
2026-03-16 14:27:34 +0900

    [level 3] Title: 단속카메라, Time: 23.28 ms, Memory: 36 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.js
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.js
@@ -1,28 +1,32 @@
-// 모든 차량이 한 번은 단속용 카메라를 만나도록 하려면 최소 몇 대의 카메라를 설치해야 하는가
+// 차례로 루트를 순회해 겹치는 구간에 대한 개수를 알아내자
 function solution(routes) {
     let answer = 1;
-    const copy = routes.map((route) => [...route]);
     
-    copy.sort((a, b) => {
+    routes = routes.sort((a, b) => {
         if(a[0] === b[0]) return a[1] - b[1];
         return a[0] - b[0];
     })
     
-    let prevEnd = copy[0][1];
-    for(let i = 1; i < copy.length; i++) {
-        const [curStart, curEnd] = copy[i];
-        
-        if(prevEnd >= curStart) {
-            if(prevEnd > curEnd) {
-                prevEnd = curEnd;
-            }
-            continue;
+    let left = routes[0][0];
+    let right = routes[0][1];
+    
+    for(let route of routes) {
+        const [start, end] = route;
+        if(right < start) {
+            answer += 1;
+            left = start;
+            right = end;
         }
         else {
-            answer += 1;
-            prevEnd = curEnd;
+            if(left < start) {
+                left = start;
+            }
+            if(right > end) {
+                right = end;
+            }
         }
     }
+    
     
     return answer;
 }
