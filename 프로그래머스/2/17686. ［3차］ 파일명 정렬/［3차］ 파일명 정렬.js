66a898474f4e5c7971c58279c8974261cf9ad017
dobby <wndqhr__@naver.com>
2026-03-16 13:35:14 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 17.42 ms, Memory: 35.9 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
@@ -1,27 +1,27 @@
+// HEAD는 숫자가 아닌 문자로 이루어짐
+// NUMBER는 연속된 숫자로 이루어짐(최대 다섯 글자)
+// TAIL은 나머지 부분
 function solution(files) {
+    // 문자열 비교시 대소문자 구분을 하지 않는다.
     let answer = [];
+    const regex = /^([^0-9]+)([0-9]{1,5})(.*)$/
+    let list = [];
     
-    let modify_files = files.map((file, idx) => {
-        // head, number, tail 구분
-        let split_file_data = file.split(/(\d+)/);
-        // 대소문자를 구분하지 않기 위해 소문자로 변경
-        let head = split_file_data[0].toLowerCase();
-        let number = parseInt(split_file_data[1]);
+    for(let file of files) {
+        const matches = file.match(regex);
         
-        return [head, number, idx];
-    })
-        
-    modify_files.sort((a, b) => {
-        if(a[0] === b[0]) {
-            return a[1] - b[1];
-        } else {
-            return a[0].localeCompare(b[0]);
+        if(matches) {
+            const [full, head, number, tail] = matches;
+            list.push([head, number, tail]);
         }
-    });
+    }
     
-    modify_files.forEach(([head, number, idx]) => {
-        answer.push(files[idx]);
+    list = list.sort((a, b) => {
+        if(a[0].toLowerCase() === b[0].toLowerCase()) {
+            return Number(a[1]) - Number(b[1]);
+        }
+        return a[0].toLowerCase().localeCompare(b[0].toLowerCase());
     })
-    
-    return answer;
+            
+    return list.map((item) => item.join(''));
 }
