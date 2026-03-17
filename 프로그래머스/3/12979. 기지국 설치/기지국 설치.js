565ed026f8b5e46bab4dae5437d3072017c94b01
dobby <wndqhr__@naver.com>
2026-03-17 14:47:49 +0900

    [level 3] Title: 기지국 설치, Time: 2.03 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.js
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.js
@@ -1,25 +1,26 @@
 function solution(n, stations, w) {
     let answer = 0;
-    let idx = 1;
-    const divide = 2 * w + 1;
+    const range = w * 2 + 1;
     
-    stations.forEach((station) => {
-        let left = station - w - 1;
-        let right = station + w + 1;
+    for(let i = 0; i < stations.length; i++) {
+        let diff = 0;
         
-        const range = left - idx + 1;
-        const cnt = Math.ceil(range / divide);
-        answer += cnt;
+        if(i === 0) {
+            diff = stations[i] - w - 1;
+        } else {
+            diff = (stations[i] - stations[i-1]) - 2 * w - 1;
+        }
         
-        idx = right;
-    })
-    
-    if(idx <= n) {
-        const lastStation = stations[stations.length - 1];
-        const right = lastStation + w + 1;
-        const range = n - right + 1;
-        answer += Math.ceil(range / divide)
+        if(diff < 0) continue;
+        
+        answer += Math.ceil(diff / (w * 2 + 1));
     }
+    
+    // 마지막
+    if((n - stations[stations.length - 1] - w) > 0) {
+        answer += Math.ceil((n - stations[stations.length - 1] - w) / (w * 2 + 1))
+    }
+    
 
     return answer;
 }
