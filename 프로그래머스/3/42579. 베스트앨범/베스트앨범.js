de1ccd372b69e60315a0fddf41482ed4d90752a6
dobby <wndqhr__@naver.com>
2026-03-18 15:17:31 +0900

    [level 3] Title: 베스트앨범, Time: 0.43 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -1,34 +1,41 @@
 function solution(genres, plays) {
     let answer = [];
-    const album = {};
-    const playCnt = {};
     
-    genres.forEach((genre, idx) => {
-        if(album[genre] === undefined) {
-            playCnt[genre] = plays[idx];
-            album[genre] = [[idx, plays[idx]]];
-        }
-        else {
-            playCnt[genre] += plays[idx];
-            album[genre].push([idx, plays[idx]]);
-        }
-    })
+    // map으로 genre별 얼마나 재생됐는지 확인
+    // map으로 genre별 [고유 번호, 재생 횟수]를 묶음
+    // 내림차순으로 정렬해 genre별 재생 횟수가 많은 것부터 탐색
+    // genre별 [고유 번호, 재생 횟수] 리스트를 정렬해 재생 횟수가 높은 것부터 차례로 최대 2개만 저장
     
-    const playCntArr = [];
-    for(let [key, value] of Object.entries(playCnt)) {
-        playCntArr.push([key, value]);
-    }
-    playCntArr.sort((a, b) => b[1] - a[1]);
+    const genrePlayMap = new Map();
+    const genreMap = new Map();
     
-    playCntArr.forEach(([genre, cnt]) => {
-        const genrePlayList = album[genre];
-        genrePlayList.sort((a, b) => b[1] - a[1]);
+    for(let i = 0; i < genres.length; i++) {
+        const genre = genres[i];
+        const play = plays[i];
         
-        genrePlayList.forEach((play, idx) => {
-            if(idx >= 2) return;
-            answer.push(play[0]);
-        })
-    })
+        if(!genrePlayMap.has(genre)) genrePlayMap.set(genre, 0);
+        if(!genreMap.has(genre)) genreMap.set(genre, []);
+        
+        genrePlayMap.set(genre, genrePlayMap.get(genre) + play);
+        const genreArr = genreMap.get(genre);
+        genreArr.push([i, play]);
+        genreMap.set(genre, genreArr);
+    }
+    
+    const sortedGenreMap = new Map([...genrePlayMap].sort((a, b) => b[1] - a[1]));
+    
+    for(let [key, value] of sortedGenreMap) {
+        let list = genreMap.get(key);
+        
+        if(list.length === 1) answer.push(list[0][0]);
+        else {
+            list = list.sort((a, b) => {
+                if(b[1] === a[1]) return a[0] - b[0];
+                return b[1] - a[1]
+            });
+            answer.push(list[0][0], list[1][0]);
+        }
+    }
     
     return answer;
 }
