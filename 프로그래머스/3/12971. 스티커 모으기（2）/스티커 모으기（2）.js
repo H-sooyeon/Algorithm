a04f4b0faf41beb6eb7d5ec6ea80448eddf95348
dobby <wndqhr__@naver.com>
2026-03-18 19:51:19 +0900

    [level 3] Title: 스티커 모으기(2), Time: 6.07 ms, Memory: 39.3 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.js
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.js
@@ -1,28 +1,22 @@
+// sticker를 하나 더 이어붙여 원형 모양 스티커 계산을 더 쉽게 함
+// dp로 어떤걸 뜯을 것인지 판단 dp[i] = max(dp[i-2] + sticker[i], dp[i-1])
 function solution(sticker) {
-    const len = sticker.length;
-    const firstStart = new Array(len).fill(0);
-    const secondStart = new Array(len).fill(0);
+    const firstDp = new Array(sticker.length).fill(0); // 첫 인덱스 뜯음
+    const secondDp = new Array(sticker.length).fill(0);// 첫 인덱스 뜯지 않음
+    const lastIdx = sticker.length - 1;
     
-    if(sticker.length <= 2) {
-        if(sticker.length === 1) return sticker[0];
-        return Math.max(sticker[0], sticker[1]);
+    if(sticker.length === 1) return sticker[0];
+    if(sticker.length === 2) return Math.max(sticker[0], sticker[1]);
+    
+    firstDp[0] = sticker[0];
+    firstDp[1] = sticker[0];
+    secondDp[1] = sticker[1];
+    for(let i = 2; i < sticker.length - 1; i++) {
+        firstDp[i] = Math.max(firstDp[i-2] + sticker[i], firstDp[i-1]);
+        secondDp[i] = Math.max(secondDp[i-2] + sticker[i], secondDp[i-1]);
     }
     
-    firstStart[0] = sticker[0];
-    firstStart[1] = sticker[0];
-    
-    secondStart[1] = sticker[1];
-    secondStart[2] = sticker[1];
-    
-    // firstStart
-    for(let i = 2; i < len - 1; i++) {
-        firstStart[i] = Math.max(firstStart[i-1], firstStart[i-2] + sticker[i]);
-    }
-    
-    // secondStart
-    for(let i = 2; i < len; i++) {
-        secondStart[i] = Math.max(secondStart[i-1], secondStart[i-2] + sticker[i]);
-    }
+    secondDp[lastIdx] = Math.max(secondDp[lastIdx-2] + sticker[lastIdx], secondDp[lastIdx-1])
 
-    return Math.max(firstStart[len-2], secondStart[len-1]);
+    return Math.max(secondDp[lastIdx], firstDp[lastIdx - 1]);
 }
