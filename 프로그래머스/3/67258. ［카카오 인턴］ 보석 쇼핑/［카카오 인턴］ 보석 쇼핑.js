c8a8cb6d4567d908ed28d72f2509405af60790a5
dobby <wndqhr__@naver.com>
2026-03-23 23:56:33 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 34.68 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
@@ -1,41 +1,37 @@
 function solution(gems) {
     let answer = [1, gems.length];
-    const uniqueJewel = new Set(gems);
-    const jewelMap = new Map();
+    const uniqueGems = new Set(gems);
     
-    if(uniqueJewel.size === 1) {
-        return [1, 1];
-    }
+    if(uniqueGems.size === 1) return [1, 1];
+    if(gems.length === uniqueGems.size) return [1, gems.length];
     
-    let startIdx = 0;
-    gems.forEach((gem, idx) => {
-        if(jewelMap.get(gem) !== undefined) {
-            jewelMap.set(gem, jewelMap.get(gem) + 1);
-            
-            // 앞에 거 확인
-            let deletedJewelIdx = startIdx;
-            for(let i = startIdx; i < idx; i++) {
-                const jewel = gems[i];
-                if(jewelMap.get(jewel) > 1) {
-                    jewelMap.set(jewel, jewelMap.get(jewel) - 1);
-                    deletedJewelIdx += 1;
-                }
-                else {
-                    startIdx = deletedJewelIdx;
-                    break;
-                }
-            }
+    let left = 0;
+    let right = 0;
+    
+    const map = new Map();
+    map.set(gems[0], 1);
+    
+    while(right < gems.length && left < gems.length) {
+        if(map.size < uniqueGems.size) {
+            right += 1;
+            map.set(gems[right], (map.get(gems[right]) || 0) + 1);
         }
         else {
-            jewelMap.set(gem, 1);
-        }
-        
-        if(jewelMap.size === uniqueJewel.size) {
-            if(answer[1] - answer[0] > idx - startIdx) {
-                answer = [startIdx + 1, idx + 1];
+            if(map.get(gems[left]) >= 1) {
+                const diff = right - left;
+                const answerDiff = answer[1] - answer[0];
+                if((diff < answerDiff) || (diff === answerDiff) && left < answer[0]) {
+                    answer = [left + 1, right + 1];
+                }
+                map.set(gems[left], map.get(gems[left]) - 1);
             }
+            if(map.get(gems[left]) === 0) {
+                map.delete(gems[left]);
+            }
+            
+            left += 1;
         }
-    })
+    }
     
     return answer;
 }
