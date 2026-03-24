e49610c7f0e73631c1fc3b8be9cf5cc71c30453e
dobby <wndqhr__@naver.com>
2026-03-24 23:52:53 +0900

    [level 3] Title: 징검다리 건너기, Time: 18.35 ms, Memory: 40.9 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.js
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.js
@@ -1,33 +1,32 @@
 function solution(stones, k) {
     let answer = 0;
     
-    let low = 1;
-    let high = 200000000;
+    let left = 1;
+    let right = 200000000;
     
-    while(low <= high) {
-        const mid = Math.floor((low + high) / 2);
+    while(left <= right) {
+        const mid = Math.floor((left + right) / 2);
         
-        let empty = 0;
+        let cnt = 0;
+        let canPass = true;
         for(let i = 0; i < stones.length; i++) {
-            if(stones[i] - mid <= 0) {
-                empty += 1;
+            if(stones[i] - mid < 0) {
+                cnt += 1;
             }
-            else {
-                empty = 0;
-            }
+            else cnt = 0;
             
-            if(empty >= k) {
+            if(cnt >= k) {
+                canPass = false;
                 break;
             }
         }
         
-        if(empty >= k) {
-            high = mid - 1;
-            answer = mid;
+        if(!canPass) {
+            right = mid - 1;
         }
         else {
-            low = mid + 1;
-            // answer = low;
+            answer = mid;
+            left = mid + 1;
         }
     }
     
