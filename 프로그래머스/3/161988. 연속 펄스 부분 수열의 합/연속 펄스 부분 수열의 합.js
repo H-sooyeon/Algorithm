40fec83ea1845d30da76c1b73a8f86ce2328f09e
dobby <wndqhr__@naver.com>
2026-03-25 16:14:18 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 23.33 ms, Memory: 63.2 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.js
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.js
@@ -1,42 +1,18 @@
 function solution(sequence) {
-    let answer = 0;
+    const prefix = [0];
     
-    const minusSequence = sequence.map((value, idx) => {
-        if(idx % 2 === 0) return value * -1;
-        return value;
-    })
-    
-    const plusSequence = sequence.map((value, idx) => {
-        if(idx % 2 === 0) return value;
-        return value * -1;
-    })
-    
-    for(let i = 1; i < sequence.length; i++) {
-        minusSequence[i] = minusSequence[i-1] + minusSequence[i];
-        plusSequence[i] = plusSequence[i-1] + plusSequence[i];
-    }
-        
-    const findMaxValue = (seq) => {        
-        let minIdx = 0;
-        let maxIdx = 0;
-        
-        let maxValue = seq[0];
-        for(let i = 1; i < seq.length; i++) {
-            maxIdx = i;
-            if(seq[i] < seq[minIdx]) {
-                minIdx = i;
-                maxValue = Math.max(maxValue, seq[minIdx]);
-                continue;
-            }
-            
-            maxValue = Math.max(maxValue, seq[maxIdx] - seq[minIdx], seq[i]);
-        }
-        
-        return maxValue;
+    let pulse = 1;
+    for(let i = 0; i < sequence.length; i++) {
+        prefix.push(prefix[i] + sequence[i] * pulse);
+        pulse *= -1;
     }
     
-    const minusMaxValue = findMaxValue(minusSequence);
-    const plusMaxValue = findMaxValue(plusSequence);
-        
-    return Math.max(minusMaxValue, plusMaxValue);
+    let minVal = prefix[0];
+    let maxVal = prefix[0];
+    for(let i = 0; i < prefix.length; i++) {
+        if(minVal > prefix[i]) minVal = prefix[i];
+        if(maxVal < prefix[i]) maxVal = prefix[i];
+    }
+    
+    return maxVal - minVal;
 }
