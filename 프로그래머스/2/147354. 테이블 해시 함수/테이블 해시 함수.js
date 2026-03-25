616d0fa5c6c52637d5993d99e060d1b9200c8e8b
dobby <wndqhr__@naver.com>
2026-03-25 15:35:30 +0900

    [level 2] Title: 테이블 해시 함수, Time: 14.11 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.js
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.js
@@ -1,29 +1,25 @@
-function solution(data, col, row_begin, row_end) {
-    let answer = 0;
-    // col을 기준으로 오름차순 정렬
-    // S_i = i번째 행의 튜플에 대해 각 컬럼의 값을 i로 나눈 나머지들의 합
-    // i는 row_begin <= i <= row_end
-    
-    data.sort((a, b) => {
-        if(a[col-1] === b[col-1]) {
-            return b[0] - a[0];
+function solution(data, col, row_begin, row_end) {    
+    const hashFunc = (col, row_begin, row_end) => {
+        const sortedData = data.sort((a, b) => {
+            if(a[col-1] === b[col-1]) {
+                return b[0] - a[0];
+            }
+            return a[col-1] - b[col-1];
+        })
+        
+        let s = 0;
+        for(let i = 0; i < data.length; i++) {
+            let s_i = 0;
+            for(let j = 0; j < data[i].length; j++) {
+                s_i += data[i][j] % (i + 1)
+            }
+            
+            if(i + 1 >= row_begin && i + 1 <= row_end) {
+                s ^= s_i;
+            }
         }
-        else return a[col-1] - b[col-1];
-    });
-    
-    let sum = [];
-    for(let i = row_begin-1; i < row_end; i++) {
-        let value = data[i].reduce((acc, cur) => acc + cur % (i + 1), 0)
-        sum.push(value);
+        return s;
     }
     
-    // console.log(sum);
-    // console.log(data);
-    
-    answer = sum[0];
-    for(let i = 0; i < sum.length - 1; i++) {
-        answer = answer ^ sum[i + 1];
-    }
-    
-    return answer;
+    return hashFunc(col, row_begin, row_end);
 }
