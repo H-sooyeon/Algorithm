49e600930e04615b4af56e5e89ec08c1f7a5f0a3
dobby <wndqhr__@naver.com>
2026-03-25 15:10:02 +0900

    [level 3] Title: 부대복귀, Time: 3195.91 ms, Memory: 331 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.js
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.js
@@ -1,67 +1,95 @@
-class Queue {
+// 다익스트라
+// 플로이드워셜은 노드의 총 개수가 500개 이하일 때만 가능
+class MinHeap {
     constructor() {
-        this.items = {};
-        this.tail = 0;
-        this.head = 0;
+        this.heap = [];
     }
     push(item) {
-        this.items[this.tail] = item;
-        this.tail += 1;
+        this.heap.push(item);
+        this.bubbleUp();
     }
     pop() {
-        delete this.items[this.head];
-        this.head += 1;
+        if(this.heap.length === 1) return this.heap.pop();
+        const top = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this.bubbleDown();
+        return top;
     }
-    size() {
-        return this.tail - this.head;
+    bubbleUp() {
+        let index = this.heap.length - 1;
+        while(index > 0) {
+            const parentIdx = Math.floor((index - 1) / 2);
+            if(this.heap[parentIdx][0] < this.heap[index][0]) break;
+            
+            [this.heap[parentIdx], this.heap[index]] = [this.heap[index], this.heap[parentIdx]];
+            index = parentIdx;
+        }
     }
-    front() {
-        return this.items[this.head];
+    bubbleDown() {
+        let index = 0;
+        const len = this.heap.length;
+        while(true) {
+            const left = index * 2 + 1;
+            const right = index * 2 + 1;
+            let smallest = index;
+            
+            if(left < len && this.heap[left][0] < this.heap[smallest][0]) {
+                smallest = left;
+            }
+            if(right < len && this.heap[right][0] < this.heap[smallest][0]) {
+                smallest = right;
+            }
+            if(smallest === index) break;
+            
+            [this.heap[index], this.heap[smallest]] = [this.heap[smallest], this.heap[index]];
+            index = smallest;
+        }
+    }
+    isEmpty() {
+        return this.heap.length === 0;
     }
 }
-
 function solution(n, roads, sources, destination) {
-    const INF = 987654321;
     let answer = [];
-    // destination으로 이동해야 한다.
-    // 각 부대원이 위치한 sources를 시작으로, roads를 참고해 destination으로 이동해야 한다.
-    // 목적지부터 시작
+    const MAX = 100001;
+    const INF = Number.MAX_SAFE_INTEGER;
+    const adj = Array.from({length: MAX}, () => []);
+    const distance = new Array(MAX).fill(INF);
     
-    const tree = Array.from({length: n + 1}, () => Array());
-    const memo = new Array(n + 1).fill(INF);
-    memo[destination] = 0;
-    
-    for(const road of roads) {
+    for(let road of roads) {
         const [a, b] = road;
-        tree[a].push(b);
-        tree[b].push(a);
+        adj[a].push([b, 1]);
+        adj[b].push([a, 1]);
     }
     
-    const queue = new Queue();
-    queue.push([destination, 0]); // 목적지부터 시작, 시간
-    
-    while(queue.size() > 0) {
-        const [cur, time] = queue.front();
-        queue.pop();
+    const dijkstra = (start) => {
+        const priorityQueue = new MinHeap();
         
-        for(const next of tree[cur]) {
-            if(memo[next] === INF) {
-                queue.push([next, time + 1]);
-                memo[next] = time + 1;
+        priorityQueue.push([start, 0]);
+        distance[start] = 0;
+        
+        while(!priorityQueue.isEmpty()) {
+            const [cur, dist] = priorityQueue.pop();
+            
+            if(distance[cur] < dist) continue;
+            
+            for(let [next, nDist] of adj[cur]) {
+                const cost = dist + nDist;
+                
+                if(distance[next] > cost) {
+                    distance[next] = cost;
+                    priorityQueue.push([next, cost]);
+                }
             }
         }
     }
     
-    // console.log(memo);
+    dijkstra(destination);
     
-    sources.forEach((source) => {
-        if(memo[source] !== INF) {
-            answer.push(memo[source]);
-        }
-        else if(memo[source] === INF) {
-            answer.push(-1);
-        }
-    });
+    for(let source of sources) {
+        if(distance[source] === INF) answer.push(-1);
+        else answer.push(distance[source]);
+    }
     
     return answer;
 }
