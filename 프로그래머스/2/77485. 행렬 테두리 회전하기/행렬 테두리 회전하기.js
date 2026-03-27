fe984fbeb9bb401f66f78afe0708a36637265b68
dobby <wndqhr__@naver.com>
2026-03-27 17:27:55 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 51.00 ms, Memory: 42.8 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.js
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.js
@@ -1,51 +1,48 @@
 function solution(rows, columns, queries) {
     let answer = [];
-    const arr = Array.from({ length: rows }, (_, i) => Array.from({ length: columns }, (_, j) => i * columns + j + 1));
-    
-    for(let i = 0; i < queries.length; i++) {
-        let [y1, x1, y2, x2] = queries[i];
-        x1-=1; y1-=1; x2-=1; y2-=1;
-        let min_v = arr[y1][x1];
+    // 행렬 초기화
+    const arr = Array.from({length: rows}, (_, r) => 
+        Array.from({length: columns}, (_, c) => r * columns + c + 1)
+    );
         
-        // 상단 가로 오른쪽 방향
-        let tmp = arr[y1][x2];
-        for(let j = x2; j > x1; j--) {
-            min_v = Math.min(min_v, arr[y1][j]);
-            
-            arr[y1][j] = arr[y1][j-1]
+    for(let query of queries) {
+        const [y1, x1, y2, x2] = query.map((val) => val - 1);
+        const modifiedValues = [];
+        
+        // 1. 시작점(좌상단) 값을 하나만 미리 빼둡니다.
+        const startValue = arr[y1][x1];
+        modifiedValues.push(startValue);
+
+        // 2. 좌측 세로 (위로 당기기)
+        for(let i = y1; i < y2; i++) {
+            arr[i][x1] = arr[i+1][x1];
+            modifiedValues.push(arr[i][x1]);
+        }
+
+        // 3. 하단 가로 (왼쪽으로 당기기)
+        for(let i = x1; i < x2; i++) {
+            arr[y2][i] = arr[y2][i+1];
+            modifiedValues.push(arr[y2][i]);
+        }
+
+        // 4. 우측 세로 (아래로 당기기)
+        for(let i = y2; i > y1; i--) {
+            arr[i][x2] = arr[i-1][x2];
+            modifiedValues.push(arr[i][x2]);
+        }
+
+        // 5. 상단 가로 (오른쪽으로 당기기)
+        for(let i = x2; i > x1 + 1; i--) {
+            arr[y1][i] = arr[y1][i-1];
+            modifiedValues.push(arr[y1][i]);
         }
         
-        // 오른쪽 세로 아래 방향
-        let v = arr[y2][x2];
-        for(let j = y2; j > y1; j--) {
-            min_v = Math.min(min_v, arr[j][x2]);
-            
-            arr[j][x2] = arr[j-1][x2];
-        }
-        arr[y1+1][x2] = tmp;
+        // 6. 비어있는 y1, x1+1 자리에 처음에 빼둔 startValue를 넣습니다.
+        arr[y1][x1+1] = startValue;
         
-        // 하단 가로 왼쪽 방향
-        tmp = v;
-        v = arr[y2][x1];
-        for(let j = x1; j < x2; j++) {
-            min_v = Math.min(min_v, arr[y2][j]);
-            
-            arr[y2][j] = arr[y2][j+1];
-        }
-        arr[y2][x2-1] = tmp;
-        
-        // 왼쪽 세로 위 방향
-        tmp = v;
-        for(let j = y1; j < y2; j++) {
-            min_v = Math.min(min_v, arr[j][x1]);
-            
-            arr[j][x1] = arr[j+1][x1];
-        }
-        arr[y2-1][x1] = tmp;
-        answer.push(min_v);
+        // 최솟값 찾기
+        answer.push(Math.min(...modifiedValues));
     }
-    
-    
-    
+        
     return answer;
 }
