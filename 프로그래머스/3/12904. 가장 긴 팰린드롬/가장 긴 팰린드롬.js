68e5bedf9eb545530dba1c4451a7c8b9ec1b9deb
dobby <wndqhr__@naver.com>
2026-03-31 16:21:45 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 20.51 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.js
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.js
@@ -1,30 +1,24 @@
 function solution(s) {
     let answer = 1;
-    const len = s.length;
-    const dp = Array.from({length: len}, () => Array(len).fill(false));
     
-    for(let i = 0; i < len; i++) {
-        dp[i][i] = true;
-    }
+    if(s === s.split("").reverse().join("")) return s.length;
+    if(s.length === 1) return 1;
     
-    for(let i = 0; i < s.length - 1; i++) {
-        if(s[i] === s[i+1]) {
-            dp[i][i+1] = true;
-            answer = 2;
+    const expand = (s, left, right) => {
+        while(left >= 0 && right < s.length && s[left] === s[right]) {
+            left -= 1;
+            right += 1;
         }
+        
+        // 팰린드롬 길이 반환
+        return right - left - 1 > 0 ? right - left - 1 : 1;
     }
     
-    for(let i = 2; i < len; i++) {
-        for(let start = 0; start < len - i; start++) {
-            const end = start + i;
-            
-            if(s[start] === s[end] && dp[start+1][end-1]) {
-                dp[start][end] = true;
-                answer = Math.max(answer, i + 1);
-            }
-        }
+    for(let i = 0; i < s.length; i++) {
+        const result = Math.max(answer, expand(s, i, i));
+        answer = Math.max(answer, expand(s, i, i)) // 홀수 중심
+        answer = Math.max(answer, expand(s, i, i+1)) // 짝수 중심
     }
-    
 
-    return answer;
+    return answer; 
 }
