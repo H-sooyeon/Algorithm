4a6b64c024e68d47723e2c3653edbfddd3cc5e12
dobby <wndqhr__@naver.com>
2026-04-13 14:38:54 +0900

    [level 2] Title: N-Queen, Time: 587.18 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.js
+++ 프로그래머스/2/12952. N－Queen/N－Queen.js
@@ -1,41 +1,46 @@
-// 백트래킹
-// 해당 체스판 위치에 퀸을 놓을 수 있는지 없는지 체크
-// 놓은 퀸의 개수가 n개일 경우 cnt++
-
 function solution(n) {
+    // 퀸은 가로/세로/대각선으로 이동할 수 있다.
+    // 한 열에 하나의 퀸
+    // 한 행에 하나의 퀸
+    // 백트래킹으로 가능한 위치에 퀸을 놓고 최종 퀸의 개수 찾기
     let answer = 0;
-    let queens = [];
     
-    const isPossible = (row, col) => {
-        let check = true;
-        
-        for(let queen of queens) {
-            if(queen[0] === row || queen[1] === col) check = false;
-            if(Math.abs(queen[0] - row) - Math.abs(queen[1] - col) === 0) 
-                check = false;
-        }
-        
-        return check;
-    }
-    
-    // 놓은 퀸의 개수, y, x 위치
-    const dfs = (row) => {
-        if(row === n) {
-            answer++;
+    const findQueen = (list, curRow) => {
+        // console.log('list', list);
+        // console.log('curRow', curRow);
+        if(curRow >= n) {
+            if(list.length === n) answer += 1;
             return;
         }
         
+        // [curRow, i]
         for(let i = 0; i < n; i++) {
-            if(isPossible(row, i)) {
-                queens.push([row, i]);
-                dfs(row + 1);
-                queens.pop();    
+            let flag = true;
+            for(let [y, x] of list) {
+                if(x === i) {
+                    flag = false;
+                    break;
+                }
+                
+                // 대각선 체크
+                const dy = Math.abs(y - curRow);
+                const dx = Math.abs(x - i);
+                
+                if(dy === dx) {
+                    flag = false;
+                    break;
+                }
+            }
+            
+            if(flag) {
+                list.push([curRow, i]);
+                findQueen(list, curRow + 1)
+                list.pop();
             }
         }
     }
     
-    dfs(0);
-    
+    findQueen([], 0);
     
     return answer;
 }
