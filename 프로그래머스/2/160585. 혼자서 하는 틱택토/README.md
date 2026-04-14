aaf996cd62efec50587d6bba8b612e24b178381b
dobby <wndqhr__@naver.com>
2026-04-14 13:29:32 +0900

    [level 2] Title: 혼자서 하는 틱택토, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/160585. 혼자서 하는 틱택토/README.md
+++ 프로그래머스/2/160585. 혼자서 하는 틱택토/README.md
@@ -1,10 +1,10 @@
 # [level 2] 혼자서 하는 틱택토 - 160585 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/160585#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/160585) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.14 ms
+메모리: 33.4 MB, 시간: 0.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 10일 12:23:08
+2026년 04월 14일 13:29:28
 
 ### 문제 설명
 
--- 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.js
+++ 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.js
@@ -1,73 +1,56 @@
+// 규칙을 지켜서 나올 수 있는 게임 상황 1, 아니라면 0
+// 누군가가 이긴 상황인가?
+// 누군가의 표시가 더 많지는 않은가? (+- 1)
+// 선공은 O, 후공은 X
 function solution(board) {
-    let o_cnt = 0;
-    let o_row = false;
+    let answer = 1;
+
+    // 선공과 후공의 개수 세기
+    let first = 0;
+    let second = 0;
     
-    let x_cnt = 0;
-    let x_row = false;
-    
-    board.forEach((r) => {
-        let o_row_cnt = 0;
-        let x_row_cnt = 0;
-        
-        for(let i = 0; i < r.length; i++) {
-            if(r[i] === 'O') {
-                o_cnt++;
-                o_row_cnt++;
-            }
-            else if(r[i] === 'X') {
-                x_cnt++;
-                x_row_cnt++;
-            }
+    for(let i = 0; i < 3; i++) {
+        for(let j = 0; j < 3; j++) {
+            if(board[i][j] === 'O') first += 1;
+            else if(board[i][j] === 'X') second += 1;
         }
+    }
         
-        if(!o_row && o_row_cnt > 2) o_row = true;
-        if(!x_row && x_row_cnt > 2) x_row = true;
-    })
+    // 개수 차이에 의한 규칙 어긋남
+    if(first + 1 <= second) return 0;
+    if(first > second + 1) return 0;
     
-    // 어느 한 쪽을 더 많이 둔 경우 확인, 선공 확인
-    if(Math.abs(o_cnt - x_cnt) > 1 || o_cnt < x_cnt) return 0;
-    
-    let flag_o = false;
-    let flag_x = false;
-    // 빙고 확인은 O or X가 3개 이상일 때만
-    if(o_row) flag_o = true;
-    if(x_row) flag_x = true;
-    
-    if(o_cnt > 2 || x_cnt > 2) {
-        // 세로 빙고 확인
-        for(let i = 0; i < 3; i++) {
-            let o_col_cnt = 0;
-            let x_col_cnt = 0;
-            
-            for(let j = 0; j < 3; j++) {
-                if(board[j][i] === 'O') o_col_cnt++;
-                if(board[j][i] === 'X') x_col_cnt++;
-            }
-            
-            if(o_col_cnt > 2) flag_o = true;
-            if(x_col_cnt > 2) flag_x = true;
-        }
-        
-        // 대각선 빙고 확인
-        if(board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
-            if(board[0][0] === 'O') flag_o = true;
-            if(board[0][0] === 'X') flag_x = true;
-        }
-        if(board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
-            if(board[0][2] === 'O') flag_o = true;
-            if(board[0][2] === 'X') flag_x = true;
+    // 선/후공이 이긴 경우 확인
+    // 가로 확인
+    let firstWin = false;
+    let secondWin = false;
+    for(let row = 0; row < 3; row++) {
+        if(board[row][0] === board[row][1] && board[row][2] === board[row][0]) {
+            if(board[row][0] === 'O') firstWin = true;
+            else if(board[row][0] === 'X') secondWin = true;
         }
     }
     
-    // 게임 종료 확인
-    if(o_cnt + x_cnt === 9) {
-        if(flag_o && !flag_x && x_cnt + 1 === o_cnt) return 1;
-        return 0;
+    // 세로 확인
+    for(let col = 0; col < 3; col++) {
+        if(board[0][col] === board[1][col] && board[2][col] === board[0][col]) {
+            if(board[0][col] === 'O') firstWin = true;
+            else if(board[0][col] === 'X') secondWin = true;
+        }
     }
     
-    if(flag_x && !flag_o && x_cnt === o_cnt) return 1;
-    if(flag_o && !flag_x && x_cnt + 1 === o_cnt) return 1;
-    if(flag_x || flag_o) return 0;
-        
-    return 1;
+    // 대각선 확인
+    if(board[0][0] === board[1][1] && board[0][0] === board[2][2]) {
+        if(board[0][0] === 'O') firstWin = true;
+        else if(board[0][0] === 'X') secondWin = true;
+    }
+    if(board[0][2] === board[1][1] && board[0][2] === board[2][0]) {
+        if(board[0][2] === 'O') firstWin = true;
+        else if(board[0][2] === 'X') secondWin = true;
+    }
+    
+    if(firstWin && first !== second + 1) return 0;
+    if(secondWin && first !== second) return 0;
+    
+    return answer;
 }
