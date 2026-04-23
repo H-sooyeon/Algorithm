21d5a4668812ce6b93ec4741aa63dcd9114654d6
dobby <wndqhr__@naver.com>
2026-04-23 23:09:47 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 408.82 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.js
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.js
@@ -1,27 +1,23 @@
-// 트럭은 택배 상자를 최대 cap개 실을 수 있다.
-// 배달과 수거를 마치고 물류창고까지 돌아올 수 있는 최소 이동 거리 구하기
+// 먼 곳부터 배달
 function solution(cap, n, deliveries, pickups) {
     let answer = 0;
-    
     let delivery = 0;
     let pickup = 0;
     
-    for(let i = n-1; i >= 0; i--) {
-        let cnt = 0;
+    for(let i = n - 1; i >= 0; i--) {
+        let cnt = 0; // 방문 횟수
         
-        if(deliveries[i] > 0 || pickups[i] > 0) {
-            while(delivery < deliveries[i] || pickup < pickups[i]) {
-                cnt += 1;
-                delivery += cap;
-                pickup += cap;
-            }
-            delivery -= deliveries[i];
-            pickup -= pickups[i];
-            
-            if(cnt > 0) {
-                answer += (cnt * (i + 1) * 2);
-            }
+        // 배달 혹은 수거할 게 있는데 한 번에 가능하지 않는다면
+        while(delivery < deliveries[i] || pickup < pickups[i]) {
+            cnt += 1;
+            delivery += cap;
+            pickup += cap;
+            // 용량 확보
         }
+        
+        delivery -= deliveries[i];
+        pickup -= pickups[i];
+        answer += (i + 1) * 2 * cnt;
     }
     
     return answer;
