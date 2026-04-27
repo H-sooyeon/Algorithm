78bffb173c462dccd8f84a2cbecc921bbdad4344
dobby <wndqhr__@naver.com>
2026-04-27 22:40:45 +0900

    [level 3] Title: 광고 삽입, Time: 1015.34 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/3/72414. 광고 삽입/광고 삽입.js
+++ 프로그래머스/3/72414. 광고 삽입/광고 삽입.js
@@ -1,54 +1,55 @@
-function solution(play_time, adv_time, logs) {
-    let answer = '';
-    
+function solution(play_time, adv_time, logs) {    
     const convertTimeToSec = (time) => {
         const [hh, mm, ss] = time.split(':').map(Number);
-        
-        let convertedTime = hh * 3600 + mm * 60 + ss;
-        return convertedTime;
+        return hh * 3600 + mm * 60 + ss;
     }
     
     const convertTimeToStr = (time) => {
-        const hh = Math.floor(time / 3600);
-        const mm = Math.floor(time % 3600 / 60);
-        const ss = time % 60;
+        let hh = Math.floor(time / 3600);
+        let mm = Math.floor((time % 3600) / 60);
+        let ss = time % 60;
         
-        return `${hh < 10 ? '0': ''}${hh}:${mm < 10 ? '0' : ''}${mm}:${ss < 10 ? '0' : ''}${ss}`;
+        if(hh < 10) hh = `0${hh}`;
+        if(mm < 10) mm = `0${mm}`;
+        if(ss < 10) ss = `0${ss}`;
+        
+        return `${hh}:${mm}:${ss}`;
     }
     
-    const secPlayTime = convertTimeToSec(play_time);
-    const mediaPlayTime = new Array(secPlayTime + 1).fill(0);
-    const advSecTime = convertTimeToSec(adv_time);
+    const adPlayTime = convertTimeToSec(adv_time);
+    const playTime = convertTimeToSec(play_time);
+    const prefix = new Array(playTime + 1).fill(0);
     
-    // 시청자 재생 구간 저장
-    logs.forEach((log) => {
-        const [start, end] = log.split('-');
+    for(let log of logs) {
+        const [startTime, endTime] = log.split('-');
+        const start = convertTimeToSec(startTime);
+        const end = convertTimeToSec(endTime);
         
-        const convertedStartTime = convertTimeToSec(start);
-        const convertedEndTime = convertTimeToSec(end);
-        
-        mediaPlayTime[convertedStartTime] += 1;
-        mediaPlayTime[convertedEndTime] -= 1;
-    })
-    
-    // 1차. 각 시청자 누적합
-    for(let i = 1; i <= secPlayTime; i++) {
-        mediaPlayTime[i] += mediaPlayTime[i-1];
+        prefix[start] += 1;
+        prefix[end] -= 1;
     }
     
-    // 2차. 시청자 재생 시간 누적
-    for(let i = 1; i <= secPlayTime; i++) {
-        mediaPlayTime[i] += mediaPlayTime[i-1];
+    // 영화를 본 사람 수 누적
+    for(let i = 1; i <= playTime; i++) {
+        prefix[i] += prefix[i-1];
     }
     
-    let resultTime = 0;
-    let save = mediaPlayTime[advSecTime];
-    for(let i = advSecTime; i <= secPlayTime; i++) {
-        if(mediaPlayTime[i] - mediaPlayTime[i-advSecTime] > save) {
-            resultTime = i - advSecTime + 1;
-            save = mediaPlayTime[i] - mediaPlayTime[i-advSecTime];
+    // 영화 재생 시간 누적
+    for(let i = 1; i <= playTime; i++) {
+        prefix[i] += prefix[i-1];
+    }
+
+    let maxPrefixPlayTime = prefix[adPlayTime - 1];
+    let startTime = 0;
+    
+    for(let i = 1; i <= playTime - adPlayTime; i++) {
+        const currentPlayTime = prefix[i + adPlayTime - 1] - prefix[i - 1];
+        
+        if(currentPlayTime > maxPrefixPlayTime) {
+            maxPrefixPlayTime = currentPlayTime;
+            startTime = i;
         }
     }
-        
-    return convertTimeToStr(resultTime);
+    
+    return convertTimeToStr(startTime);
 }
