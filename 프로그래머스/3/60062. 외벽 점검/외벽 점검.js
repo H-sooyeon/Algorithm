be141a264e2a5e25367b4adc21abd4b1a868b487
dobby <wndqhr__@naver.com>
2026-05-14 17:17:20 +0900

    [level 3] Title: 외벽 점검, Time: 77.88 ms, Memory: 43.5 MB -BaekjoonHub

--- 프로그래머스/3/60062. 외벽 점검/외벽 점검.js
+++ 프로그래머스/3/60062. 외벽 점검/외벽 점검.js
@@ -1,56 +1,59 @@
+// n: 외벽의 길이
+// weak: 취약 지점 위치
+// dist: 친구가 1시간 동안 이동할 수 있는 거리가 담긴 배열
+// 취약 지점을 점검하기 위해 보내야 하는 친구 수의 최소값
 function solution(n, weak, dist) {
-    let answer = Infinity;
-    const doubleWeak = [...weak];
-    const permutation = [];
-    const visited = new Array(dist.length).fill(false);
-    
-    weak.forEach((pos) => doubleWeak.push(n + pos));
-    
-    const Permutation = (v) => {
-        if(v.length === dist.length) {
-            permutation.push([...v]);
+    let answer = -1;
+    const doubleWeak = [...weak, ...(weak.map((v) => v + n))];
+    const friends = [];
+
+    const permutation = (visited, list) => {
+        if(list.length === dist.length) {
+            friends.push([...list]);
             return;
         }
         
         for(let i = 0; i < dist.length; i++) {
             if(visited[i]) continue;
             
-            v.push(i);
             visited[i] = true;
-            Permutation(v);
-            v.pop();
+            list.push(dist[i]);
+            permutation(visited, list);
             visited[i] = false;
+            list.pop();
         }
     }
     
-    Permutation([]);
+    const visited = new Array(dist.length).fill(false);
+    permutation(visited, []);
+    // console.log(friends);
     
-    permutation.forEach((list) => {
-        for(let t = 0; t < weak.length; t++) {
-            let save = [list[0]];
-            let weakPos = t;
-            let findWeakCnt = 0;
+    for(let friend of friends) {
+        // 두배로 늘린 weak에서 각 인덱스를 시작점으로 잡아서 친구 보내기
+        for(let i = 0; i < weak.length; i++) {
             let friendIdx = 0;
-            let canMoveSum = dist[list[0]] + doubleWeak[t];
+            let weakCnt = 0;
+            let target = doubleWeak[i + weakCnt] + friend[friendIdx];
+        
+            while(weakCnt < weak.length && friendIdx < dist.length) {
+                weakCnt += 1;
             
-            while(friendIdx < dist.length && weakPos < doubleWeak.length) {
-                if(findWeakCnt === weak.length) {
-                    answer = Math.min(answer, save.length);
-                    break;
-                }
-                
-                if(canMoveSum < doubleWeak[weakPos]) {
+                // console.log(target, doubleWeak[i + weakCnt], i + weakCnt);
+                if(weakCnt < weak.length && target < doubleWeak[i + weakCnt]) {
                     friendIdx += 1;
-                    canMoveSum = doubleWeak[weakPos] + dist[list[friendIdx]];
-                    save.push(friendIdx);
+                    // console.log('friend 추가');
+                    target = (doubleWeak[i + weakCnt] + friend[friendIdx]);
                 }
-                
-                weakPos += 1;
-                findWeakCnt += 1;
+            }
+        
+            // console.log('start', i, 'weakCnt', weakCnt, 'friend', friendIdx + 1, 'target', target);
+            if(weakCnt === weak.length) {
+                answer = Math.min(answer === -1 ? dist.length : answer, friendIdx + 1);
             }
         }
-    })
+    }
     
-    if(answer === Infinity) return -1;
+    
+    
     return answer;
 }
