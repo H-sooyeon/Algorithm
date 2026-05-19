2aa11206b79c75f46daed24255c2646324d42378
dobby <wndqhr__@naver.com>
2026-05-19 16:53:56 +0900

    [level 3] Title: 카운트 다운, Time: 109.02 ms, Memory: 63 MB -BaekjoonHub

--- 프로그래머스/3/131129. 카운트 다운/카운트 다운.js
+++ 프로그래머스/3/131129. 카운트 다운/카운트 다운.js
@@ -1,56 +1,51 @@
+// 정확히 0점으로 만들어야 하는 게임
+// 싱글: 해당 수만큼 점수+
+// 더블: 해당 수 * 2
+// 트리플: 해당 수 * 3
+// 불, 아우터 불: 50
 function solution(target) {
-    const INF = 987654321;
-    let dp = Array.from({length: target + 1}, () => [0, 0]);
+    let answer = [];
+    const dp = Array.from({length: target + 1}, () => new Array(2).fill(Infinity));
+    dp[0] = [0, 0];
     
-    if(target <= 20) return [1, 1];
-            
-    for(let num = 1; num <= target; num += 1) {
-        let answer = [INF, INF];
+    for(let i = 1; i <= target; i++) {
         // 싱글
-        for(let i = 1; i <= 20; i++) {
-            const prevIdx = num - i;
-            if(prevIdx < 0) break;
+        for(let j = 1; j <= 20; j++) {
+            if(i < j) break;
             
-            const value = [dp[prevIdx][0] + 1, dp[prevIdx][1] + 1];
-            if((answer[0] === value[0] && answer[1] < value[1]) || answer[0] > value[0]) {
-                answer = value;
+            if(dp[i-j][0] + 1 < dp[i][0] || 
+               dp[i-j][0] + 1 === dp[i][0] && dp[i-j][1] + 1 > dp[i][1]) {
+                dp[i] = [dp[i-j][0] + 1, dp[i-j][1] + 1];
             }
         }
-        
         // 더블
-        for(let i = 1; i <= 20; i++) {
-            const prevIdx = num - i * 2;
-            if(prevIdx < 0) break;
+        for(let j = 1; j <= 20; j++) {
+            if(j * 2 > i) break;
             
-            const value = [dp[prevIdx][0] + 1, dp[prevIdx][1]];
-            if((answer[0] === value[0] && answer[1] < value[1]) || answer[0] > value[0]) {
-                answer = value;
+            const double = j * 2;
+            if(dp[i-double][0] + 1 < dp[i][0]) {
+                dp[i] = [dp[i-double][0] + 1, dp[i-double][1]];
             }
         }
-        
         // 트리플
-        for(let i = 1; i <= 20; i++) {
-            const prevIdx = num - i * 3;
-            if(prevIdx < 0) break;
+        for(let j = 1; j <= 20; j++) {
+            if(j * 3 > i) break;
             
-            const value = [dp[prevIdx][0] + 1, dp[prevIdx][1]];
-            if((answer[0] === value[0] && answer[1] < value[1]) || answer[0] > value[0]) {
-                answer = value;
+            const triple = j * 3;
+            if(dp[i-triple][0] + 1 < dp[i][0]) {
+                dp[i] = [dp[i-triple][0] + 1, dp[i-triple][1]];
             }
         }
-        
-        if(num >= 50) {
-            const value = [dp[num - 50][0] + 1, dp[num - 50][1] + 1];
-            if((answer[0] === value[0] && answer[1] < value[1]) || answer[0] > value[0]) {
-                answer = value;
+        // 불
+        if(i >= 50) {
+            if(dp[i-50][0] + 1 < dp[i][0] || 
+               dp[i-50][0] + 1 === dp[i][0] && dp[i-50][1] + 1 > dp[i][1]) {
+                dp[i] = [dp[i-50][0] + 1, dp[i-50][1] + 1];
             }
         }
-        
-        dp[num] = answer;
     }
     
-    // console.log(dp[target]);
-    
-    
+    // console.log(dp);
     return dp[target];
+    // [던진 다트 수, 싱글 + 불 맞춘 횟수]
 }
