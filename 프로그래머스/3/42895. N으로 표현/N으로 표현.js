2774094d60defd30a235f4f9ff2cbb186b3aef7a
dobby <wndqhr__@naver.com>
2026-05-20 16:53:31 +0900

    [level 3] Title: N으로 표현, Time: 12.36 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.js
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.js
@@ -1,38 +1,34 @@
-// N 사용횟수의 최솟값
-// 나누기 연산에서 나머지는 무시한다.
+// N을 8개까지 사용하는 경우를 모두 구한다.
 function solution(N, number) {
-    let answer = 0;
-    const dp = Array.from({length: 10}, () => new Set());
-    dp[1].add(N); // N을 하나만 썼을 때
+    if(N === number) return 1;
     
-    // N을 하나만 썼는데 number라면 return 1
-    if(dp[1] === number) {
-        return 1;
+    let answer = 0;
+    // idx: N을 몇 개 썼는지, value: 계산 결과
+    const dp = Array.from({length: 9}, () => new Set());
+    
+    for(let count = 1; count < 9; count++) {
+        dp[count].add(Number(String(N).repeat(count)));
     }
     
-    // i는 N을 몇 번 썼는가
-    for(let i = 2; i < 9; i++) {
-        const value = Number(N.toString().repeat(i));
-        dp[i].add(value);
-        
+    for(let i = 1; i < 9; i++) {
         for(let j = 1; j < i; j++) {
-            for(let num1 of dp[j]) {
-                for(let num2 of dp[i-j]) {
-                    dp[i].add(num1 + num2);
-                    dp[i].add(Math.abs(num1 - num2));
-                    dp[i].add(Math.floor(num2 / num1));
-                    dp[i].add(num1 * num2);
+            for(const arg1 of dp[j]) {
+                for(const arg2 of dp[i-j]) {
+                    // 사칙 연산
+                    dp[i].add(arg1 + arg2);
+                    dp[i].add(arg1 * arg2);
+                    dp[i].add(arg1 - arg2);
+                    if(arg2 !== 0) dp[i].add(Math.floor(arg1 / arg2));
                 }
             }
         }
     }
     
     for(let i = 1; i < 9; i++) {
-        if(dp[i].has(number)) {
-            return i;
-        }
+        if(dp[i].has(number)) return i;
     }
     
-    // 최솟값이 8보다 크면 -1을 return
     return -1;
+    // N 사용횟수의 최솟값
+    // 8보다 크면 -1 리턴
 }
