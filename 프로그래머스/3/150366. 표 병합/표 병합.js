d940eb4801f71711c84a95b1b90c74dce0d70d64
dobby <wndqhr__@naver.com>
2026-05-25 16:22:15 +0900

    [level 3] Title: 표 병합, Time: 148.78 ms, Memory: 46.8 MB -BaekjoonHub

--- 프로그래머스/3/150366. 표 병합/표 병합.js
+++ 프로그래머스/3/150366. 표 병합/표 병합.js
@@ -1,101 +1,77 @@
+// UPDATE r c value
+// UPDATE value1 value2
+// MERGE r1 c1 r2 c2
+// UNMERGE r c
+// PRINT r c
 function solution(commands) {
     let answer = [];
-    const table = Array.from({length: 51}, () => Array(51));
     
-    const makeKey = (row, col) => {
-        return `${row},${col}`;
+    function Node() {
+        this.group = new Set();
+        this.value = null;
     }
     
-    for(let i = 1; i < 51; i++) {
-        for(let j = 1; j < 51; j++) {
-            table[i][j] = [makeKey(i, j), null];
+    const graph = Array.from({length: 51}, () => Array.from({length: 51}, () => new Node()));
+    
+    commands.forEach((command) => {
+        const line = command.split(' ');
+        const cd = line[0];
+        
+        if(cd === 'UPDATE') {
+            if(line.length === 4) { // UPDATE r c value
+                let [, r, c, value] = line;
+                const group = graph[r][c].group;
+                graph[r][c].value = value;
+                
+                for(let cell of group) {
+                    const [y, x] = cell.split(',').map(Number);
+                    graph[y][x].value = value;
+                }
+            }
+            else { // UPDATE value1 value2
+                const [, value1, value2] = line;
+                for(let i = 0; i < 51; i++) {
+                    for(let j = 0; j < 51; j++) {
+                        if(graph[i][j].value === value1) {
+                            graph[i][j].value = value2;
+                        }
+                    }
+                }
+            }
         }
-    }
-    
-    const update = (command) => {
-        if(command.length === 3) {
-            let [r, c, value] = command;
-            const key = table[Number(r)][Number(c)][0];
+        else if(cd === 'MERGE') {
+            const [, r1, c1, r2, c2] = line;
             
-            for(let i = 1; i < 51; i++) {
-                for(let j = 1; j < 51; j++) {
-                    if(table[i][j][0] === key) {
-                        table[i][j][1] = value;
-                    }
-                }
+            const group = new Set([...graph[r1][c1].group, ...graph[r2][c2].group, `${r1},${c1}`, `${r2},${c2}`]);
+            let value = graph[r1][c1].value ? graph[r1][c1].value : graph[r2][c2].value;
+            
+            for(let cell of group) {
+                const [y, x] = cell.split(',').map(Number);
+                graph[y][x].value = value;
+                graph[y][x].group = group;
             }
         }
-        else if(command.length === 2) {
-            let [value1, value2] = command;
-            for(let i = 1; i < 51; i++) {
-                for(let j = 1; j < 51; j++) {
-                    if(table[i][j][1] === value1) {
-                        table[i][j][1] = value2;
-                    }
-                }
+        else if(cd === 'UNMERGE') {
+            const [, r, c] = line;
+            
+            const group = graph[r][c].group;
+            const value = graph[r][c].value;
+            
+            for(let cell of group) {
+                const [y, x] = cell.split(',').map(Number);
+                graph[y][x].group = [];
+                graph[y][x].value = null;
             }
+            
+            graph[r][c].value = value;
         }
-    }
-    
-    const merge = (command) => {
-        const [r1, c1, r2, c2] = command.map(Number);
-        
-        const key1 = table[r1][c1][0];
-        const key2 = table[r2][c2][0];
-        
-        let value = table[r2][c2][1];
-        
-        if(table[r1][c1][1] !== null) {
-            value = table[r1][c1][1];
-        }
-        
-        for(let i = 1; i < 51; i++) {
-            for(let j = 1; j < 51; j++) {
-                if(table[i][j][0] === key2) {
-                    table[i][j][0] = key1;
-                    table[i][j][1] = value;
-                }
-                if(table[i][j][0] === key1) {
-                    table[i][j][1] = value;
-                }
-            }
-        }
-    }
-    
-    const unmerge = (command) => {
-        const [r, c] = command.map(Number);
-        const key = table[r][c][0];
-        const value = table[r][c][1];
-        
-        for(let i = 1; i < 51; i++) {
-            for(let j = 1; j < 51; j++) {
-                if(table[i][j][0] === key) {
-                    table[i][j][0] = makeKey(i, j);
-                    table[i][j][1] = null;
-                }
-            }
-        }
-        table[r][c][1] = value;
-    }
-    
-    commands.forEach((item) => {
-        const [command, ...rest] = item.split(' ');
-        
-        if(command === 'UPDATE') {
-            update(rest);
-        }
-        else if(command === 'MERGE') {
-            merge(rest);
-        }
-        else if(command === 'UNMERGE') {
-            unmerge(rest);
-        }
-        else if(command === 'PRINT') {
-            const [r, c] = rest.map(Number);
-            if(table[r][c][1] === null) {
-                answer.push('EMPTY');
+        else if(cd === 'PRINT') {
+            const [, r, c] = line;
+            
+            if(graph[r][c].value) {
+                answer.push(graph[r][c].value);
             } else {
-                answer.push(table[r][c][1]);
+                answer.push('EMPTY');
             }
         }
     })
